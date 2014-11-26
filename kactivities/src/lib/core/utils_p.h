914b22ce3b0ed95d6acb52cadcd3b91f75c5dac7
Ivailo Monev <xakepa10@gmail.com>
2014-11-26 19:20:00 +0000

    fix kactivities boostrap

--- kactivities/src/lib/core/utils_p.h
+++ kactivities/src/lib/core/utils_p.h
@@ -25,7 +25,7 @@
 #include <QMutex>
 #include <QDebug>
 
-#include <KLocalizedString>
+#include <klocalizedstring.h>
 
 // Creates an async call to retrieve a value from the dbus service
 // and initializes the call watcher
