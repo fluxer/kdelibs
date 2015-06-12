9535e0ca0b88c174eafb50eb5e2bf8782a1e6af2
Ivailo Monev <xakepa10@gmail.com>
2015-06-13 02:48:11 +0300

    cmake: look for qmake last in FindQt4 module

--- cmake/modules/FindQt4.cmake
+++ cmake/modules/FindQt4.cmake
@@ -429,7 +429,7 @@ GET_FILENAME_COMPONENT(qt_install_version "[HKEY_CURRENT_USER\\Software\\trollte
 # check for qmake
 # Debian uses qmake-qt4
 # macports' Qt uses qmake-mac
-FIND_PROGRAM(QT_QMAKE_EXECUTABLE NAMES qmake qmake4 qmake-qt4 qmake-mac PATHS
+FIND_PROGRAM(QT_QMAKE_EXECUTABLE NAMES qmake4 qmake-qt4 qmake-mac qmake PATHS
   "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\4.0.0;InstallDir]/bin"
   "[HKEY_CURRENT_USER\\Software\\Trolltech\\Versions\\${qt_install_version};InstallDir]/bin"
   $ENV{QTDIR}/bin
