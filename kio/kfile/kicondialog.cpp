fluxer/kdelibs

commit 5333e9917d0c013b2fbeb841a2980f12c1a5ffe5

generic: remove deprecated API

kio/kfile/kicondialog.cpp
@@ -447,7 +447,6 @@ void KIconDialog::KIconDialogPrivate::init()
         KIconLoader::Device,
         KIconLoader::Emblem,
         KIconLoader::Emote,
-        KIconLoader::FileSystem,
         KIconLoader::International,
         KIconLoader::MimeType,
         KIconLoader::Place,

commit c93d6d45eb390d5577e4513432b73ece03c94086

kio: fix regressions since 5333e991 which caused crashes

kio/kfile/kicondialog.cpp
@@ -308,7 +308,7 @@ class KIconDialog::KIconDialogPrivate
     KIconLoader *mpLoader;
     KIconCanvas *mpCanvas;
     int mNumContext;
-    KIconLoader::Context mContextMap[ 12 ]; // must match KIcon::Context size, code has assert
+    KIconLoader::Context mContextMap[ 11 ]; // must match KIcon::Context size, code has assert
 
     bool m_bStrictIconSize, m_bLockUser, m_bLockCustomDir;
     QString custom;
@@ -433,7 +433,6 @@ void KIconDialog::KIconDialogPrivate::init()
         I18N_NOOP( "Devices" ),
         I18N_NOOP( "Emblems" ),
         I18N_NOOP( "Emotes" ),
-        I18N_NOOP( "Filesystems" ),
         I18N_NOOP( "International" ),
         I18N_NOOP( "Mimetypes" ),
         I18N_NOOP( "Places" ),