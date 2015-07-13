0f11be04d228aafc2da379de94ba26b108543e71
Ivailo Monev <xakepa10@gmail.com>
2015-07-13 20:06:23 +0300

    kparts: make mainwindow header guard less generic
    this fixes the filelight (from kde-extraaps) build because it does
    the same thing for its own header.

--- kparts/mainwindow.h
+++ kparts/mainwindow.h
@@ -17,8 +17,8 @@
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.
 */
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef KPARTSMAINWINDOW_H
+#define KPARTSMAINWINDOW_H
 
 #include <kaction.h>
 
