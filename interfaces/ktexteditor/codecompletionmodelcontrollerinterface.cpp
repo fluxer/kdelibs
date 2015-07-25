c852b56374059f3f814a4dc87faf3e9ab49bb3a1
Ivailo Monev <xakepa10@gmail.com>
2015-07-25 18:30:26 +0300

    interfaces: interfaces: merge all CodeCompletionModelControllerInterface APIs into single one
    kate itself implements version 4 of the same interfaces so that
    is deffinetly something that needs to be merged but for now that
    will do

--- interfaces/ktexteditor/codecompletionmodelcontrollerinterface.cpp
+++ interfaces/ktexteditor/codecompletionmodelcontrollerinterface.cpp
@@ -26,8 +26,6 @@
 
 namespace KTextEditor {
 
-//BEGIN OLD  
-  
 CodeCompletionModelControllerInterface::CodeCompletionModelControllerInterface()
 {
 }
@@ -72,20 +70,22 @@ Range CodeCompletionModelControllerInterface::completionRange(View* view, const 
     return Range(start, end);
 }
 
-void CodeCompletionModelControllerInterface::updateCompletionRange(View* view, SmartRange& range)
+Range CodeCompletionModelControllerInterface::updateCompletionRange(View* view, const Range& range)
 {
-    Q_UNUSED(view);
-    if(!range.text().isEmpty() && range.text().count() == 1 && range.text().first().trimmed().isEmpty())
+    QStringList text=view->document()->textLines(range,false);
+    if(!text.isEmpty() && text.count() == 1 && text.first().trimmed().isEmpty())
       //When inserting a newline behind an empty completion-range,, move the range forward to its end
-      range.start() = range.end();
+      return Range(range.end(),range.end());
+
+    return range;
 }
 
-QString CodeCompletionModelControllerInterface::filterString(View* view, const SmartRange &range, const Cursor &position)
+QString CodeCompletionModelControllerInterface::filterString(View* view, const Range &range, const Cursor &position)
 {
     return view->document()->text(KTextEditor::Range(range.start(), position));
 }
 
-bool CodeCompletionModelControllerInterface::shouldAbortCompletion(View* view, const SmartRange &range, const QString &currentCompletion)
+bool CodeCompletionModelControllerInterface::shouldAbortCompletion(View* view, const Range &range, const QString &currentCompletion)
 {
     //kDebug()<<view->cursorPosition();
     //kDebug()<<range;
@@ -102,106 +102,15 @@ void CodeCompletionModelControllerInterface::aborted(KTextEditor::View* view) {
 }
 
 bool CodeCompletionModelControllerInterface::shouldExecute(const QModelIndex& index, QChar inserted) {
-  Q_UNUSED(index);
-  Q_UNUSED(inserted);
-  return false;
-}
-
-KTextEditor::CodeCompletionModelControllerInterface2::MatchReaction CodeCompletionModelControllerInterface2::matchingItem(const QModelIndex& selected) {
-  Q_UNUSED(selected)
-  return HideListIfAutomaticInvocation;
-}
-
-//END OLD
-
-//BEGIN V3
-
-CodeCompletionModelControllerInterface3::CodeCompletionModelControllerInterface3()
-{
-}
-
-CodeCompletionModelControllerInterface3::~CodeCompletionModelControllerInterface3()
-{
-}
-
-bool CodeCompletionModelControllerInterface3::shouldStartCompletion(View* view, const QString &insertedText, bool userInsertion, const Cursor &position)
-{
-    Q_UNUSED(view);
-    Q_UNUSED(position);
-    if(insertedText.isEmpty())
-        return false;
-
-    QChar lastChar = insertedText.at(insertedText.count() - 1);
-    if ((userInsertion && (lastChar.isLetter() || lastChar.isNumber() || lastChar == '_')) ||
-        lastChar == '.' || insertedText.endsWith(QLatin1String("->"))) {
-        return true;
-    }
+    Q_UNUSED(index);
+    Q_UNUSED(inserted);
     return false;
 }
 
-Range CodeCompletionModelControllerInterface3::completionRange(View* view, const Cursor &position)
-{
-    Cursor end = position;
-
-    QString text = view->document()->line(end.line());
-
-    static QRegExp findWordStart( "\\b([_\\w]+)$" );
-    static QRegExp findWordEnd( "^([_\\w]*)\\b" );
-
-    Cursor start = end;
-
-    if (findWordStart.lastIndexIn(text.left(end.column())) >= 0)
-        start.setColumn(findWordStart.pos(1));
-
-    if (findWordEnd.indexIn(text.mid(end.column())) >= 0)
-        end.setColumn(end.column() + findWordEnd.cap(1).length());
-
-    //kDebug()<<"returning:"<<Range(start,end);
-    return Range(start, end);
+KTextEditor::CodeCompletionModelControllerInterface::MatchReaction CodeCompletionModelControllerInterface::matchingItem(const QModelIndex& selected) {
+    Q_UNUSED(selected)
+    return HideListIfAutomaticInvocation;
 }
-
-Range CodeCompletionModelControllerInterface3::updateCompletionRange(View* view, const Range& range)
-{
-    QStringList text=view->document()->textLines(range,false);
-    if(!text.isEmpty() && text.count() == 1 && text.first().trimmed().isEmpty())
-      //When inserting a newline behind an empty completion-range,, move the range forward to its end
-      return Range(range.end(),range.end());
-    
-    return range;
-}
-
-QString CodeCompletionModelControllerInterface3::filterString(View* view, const Range &range, const Cursor &position)
-{
-    return view->document()->text(KTextEditor::Range(range.start(), position));
-}
-
-bool CodeCompletionModelControllerInterface3::shouldAbortCompletion(View* view, const Range &range, const QString &currentCompletion)
-{
-    //kDebug()<<view->cursorPosition();
-    //kDebug()<<range;
-    if(view->cursorPosition() < range.start() || view->cursorPosition() > range.end())
-      return true; //Always abort when the completion-range has been left
-    //Do not abort completions when the text has been empty already before and a newline has been entered
-
-    static const QRegExp allowedText("^(\\w*)");
-    return !allowedText.exactMatch(currentCompletion);
-}
-
-void CodeCompletionModelControllerInterface3::aborted(KTextEditor::View* view) {
-    Q_UNUSED(view);
-}
-
-bool CodeCompletionModelControllerInterface3::shouldExecute(const QModelIndex& index, QChar inserted) {
-  Q_UNUSED(index);
-  Q_UNUSED(inserted);
-  return false;
-}
-
-KTextEditor::CodeCompletionModelControllerInterface3::MatchReaction CodeCompletionModelControllerInterface3::matchingItem(const QModelIndex& selected) {
-  Q_UNUSED(selected)
-  return HideListIfAutomaticInvocation;
-}
-//END V3
 
 
 }
