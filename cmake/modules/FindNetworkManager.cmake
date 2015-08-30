757e84e13a1ed8c0d5be01a429244ecf7ae2c8e6
Ivailo Monev <xakepa10@gmail.com>
2015-08-30 22:58:38 +0300

    cmake: make NetworkManager module check for libnm-glib
    this should fix kde-workspace build against recent versions of
    NetworkManager

--- cmake/modules/FindNetworkManager.cmake
+++ cmake/modules/FindNetworkManager.cmake
@@ -9,15 +9,16 @@
 
 # Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
 # Copyright (c) 2007, Will Stephenson, <wstephenson@kde.org>
+# Copyright (c) 2015, Ivailo Monev, <xakepa10@gmail.com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 
-IF (NETWORKMANAGER_INCLUDE_DIRS AND NM-UTIL_INCLUDE_DIRS)
+IF (NETWORKMANAGER_INCLUDE_DIRS AND NM-UTIL_INCLUDE_DIRS AND NM-GLIB_INCLUDE_DIRS)
    # in cache already
    SET(NetworkManager_FIND_QUIETLY TRUE)
-ENDIF (NETWORKMANAGER_INCLUDE_DIRS AND NM-UTIL_INCLUDE_DIRS)
+ENDIF ()
 
 IF (NOT WIN32)
    # use pkg-config to get the directories and then use these values
@@ -25,18 +26,20 @@ IF (NOT WIN32)
    find_package(PkgConfig)
    PKG_SEARCH_MODULE( NETWORKMANAGER NetworkManager )
    PKG_SEARCH_MODULE( NM-UTIL libnm-util )
+   PKG_SEARCH_MODULE( NM-GLIB libnm-glib )
 ENDIF (NOT WIN32)
 
-IF (NETWORKMANAGER_FOUND AND NM-UTIL_FOUND)
+IF (NETWORKMANAGER_FOUND AND NM-UTIL_FOUND AND NM-GLIB_FOUND)
    IF (NOT NetworkManager_FIND_QUIETLY)
       MESSAGE(STATUS "Found NetworkManager ${NETWORKMANAGER_VERSION}: ${NETWORKMANAGER_LIBRARY_DIRS}")
       MESSAGE(STATUS "Found libnm-util: ${NM-UTIL_LIBRARY_DIRS}")
+      MESSAGE(STATUS "Found libnm-glib: ${NM-GLIB_LIBRARY_DIRS}")
    ENDIF (NOT NetworkManager_FIND_QUIETLY)
-ELSE (NETWORKMANAGER_FOUND AND NM-UTIL_FOUND)
+ELSE ()
    IF (NetworkManager_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could NOT find NetworkManager or libnm-util, check FindPkgConfig output above!")
+      MESSAGE(FATAL_ERROR "Could NOT find NetworkManager, libnm-util or libnm-glib, check FindPkgConfig output above!")
    ENDIF (NetworkManager_FIND_REQUIRED)
-ENDIF (NETWORKMANAGER_FOUND AND NM-UTIL_FOUND)
+ENDIF ()
 
-MARK_AS_ADVANCED(NETWORKMANAGER_INCLUDE_DIRS NM-UTIL_INCLUDE_DIRS)
+MARK_AS_ADVANCED(NETWORKMANAGER_INCLUDE_DIRS NM-UTIL_INCLUDE_DIRS NM-GLIB_INCLUDE_DIRS)
 
