263b5bf1c9c36509bba9b44d702e75a44461c656
Ivailo Monev <xakepa10@gmail.com>
2015-09-03 05:08:47 +0300

    generic: misc cleanups

--- cmake/modules/KDE4Defaults.cmake
+++ cmake/modules/KDE4Defaults.cmake
@@ -1,32 +1,39 @@
-# support for Dart: http://public.kitware.com/dashboard.php?name=kde
-if (EXISTS ${CMAKE_SOURCE_DIR}/CTestConfig.cmake)
-   include(CTest)
-endif (EXISTS ${CMAKE_SOURCE_DIR}/CTestConfig.cmake)
+# support for Dart: https://open.cdash.org/
+if(EXISTS ${CMAKE_SOURCE_DIR}/CTestConfig.cmake)
+    include(${CMAKE_SOURCE_DIR}/CTestConfig.cmake)
+    include(CTest)
+endif()
 
-# let cmake handle mocking
+# support for easy packaging
+if(EXISTS ${CMAKE_SOURCE_DIR}/CPackConfig.cmake)
+    include(${CMAKE_SOURCE_DIR}/CPackConfig.cmake)
+    include(CPack)
+endif()
+
+# let cmake handle mocking and UI compiling
 # since cmake 2.8.6
 set(CMAKE_AUTOMOC ON)
-# set(CMAKE_AUTORCC ON)
+# since cmake 3.0.0
 set(CMAKE_AUTOUIC ON)
 
-# Always include srcdir and builddir in include path
-# This saves typing ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} in about every subdir
+# Always include srcdir and builddir in include path, this saves typing
+# ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} in about every subdir
 # since cmake 2.4.0
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 
-# put the include dirs which are in the source or build tree
-# before all other include dirs, so the headers in the sources
-# are preferred over the already installed ones
+# put the include dirs which are in the source or build tree before all other
+# include dirs, so the headers in the sources are preferred over the already
+# installed ones
 # since cmake 2.4.1
 set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE ON)
 
-# define the generic version of the libraries here
-# this makes it easy to advance it when the next KDE release comes
-# Use this version number for libraries which are at version n in KDE version n
+# define the generic version of the libraries here, this makes it easy to
+# advance it when the next KDE release comes. Use this version number for
+# libraries
 set(GENERIC_LIB_VERSION "4.17.0")
 set(GENERIC_LIB_SOVERSION "4")
 
-# Use this version number for libraries which are already at version n+1 in KDE version n
+# Use this version number for libraries which are experimental
 set(KDE_NON_GENERIC_LIB_VERSION "5.15.0")
 set(KDE_NON_GENERIC_LIB_SOVERSION "5")
 
--- cmake/modules/KDE4Macros.cmake
+++ cmake/modules/KDE4Macros.cmake
@@ -20,129 +20,128 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 
-macro (KDE4_ADD_KCFG_FILES _sources )
-   foreach (_current_ARG ${ARGN})
-       if( ${_current_ARG} STREQUAL "GENERATE_MOC" )
-           set(_kcfg_generatemoc TRUE)
-       endif( ${_current_ARG} STREQUAL "GENERATE_MOC" )
+macro(KDE4_ADD_KCFG_FILES _sources )
+    foreach(_current_ARG ${ARGN})
+        if(${_current_ARG} STREQUAL "GENERATE_MOC" )
+            set(_kcfg_generatemoc TRUE)
+        endif()
 
-       if( ${_current_ARG} STREQUAL "USE_RELATIVE_PATH" )
-           set(_kcfg_relativepath TRUE)
-       endif( ${_current_ARG} STREQUAL "USE_RELATIVE_PATH" )
-   endforeach (_current_ARG ${ARGN})
+        if(${_current_ARG} STREQUAL "USE_RELATIVE_PATH" )
+            set(_kcfg_relativepath TRUE)
+        endif()
+    endforeach()
 
-   foreach (_current_FILE ${ARGN})
+    foreach(_current_FILE ${ARGN})
+        if(NOT ${_current_FILE} STREQUAL "GENERATE_MOC" AND NOT ${_current_FILE} STREQUAL "USE_RELATIVE_PATH")
+            get_filename_component(_tmp_FILE ${_current_FILE} ABSOLUTE)
+            get_filename_component(_abs_PATH ${_tmp_FILE} DIRECTORY)
 
-     if(NOT ${_current_FILE} STREQUAL "GENERATE_MOC" AND NOT ${_current_FILE} STREQUAL "USE_RELATIVE_PATH")
-       get_filename_component(_tmp_FILE ${_current_FILE} ABSOLUTE)
-       get_filename_component(_abs_PATH ${_tmp_FILE} DIRECTORY)
+            if(_kcfg_relativepath) # Process relative path only if the option was set
+                # Get relative path
+                get_filename_component(_rel_PATH ${_current_FILE} DIRECTORY)
 
-       if (_kcfg_relativepath) # Process relative path only if the option was set
-           # Get relative path
-           get_filename_component(_rel_PATH ${_current_FILE} DIRECTORY)
+                if(IS_ABSOLUTE ${_rel_PATH})
+                    # We got an absolute path
+                    set(_rel_PATH "")
+                endif()
+            endif()
 
-           if (IS_ABSOLUTE ${_rel_PATH})
-               # We got an absolute path
-               set(_rel_PATH "")
-           endif (IS_ABSOLUTE ${_rel_PATH})
-       endif (_kcfg_relativepath)
+            get_filename_component(_basename ${_tmp_FILE} NAME_WE)
+            # If we had a relative path and we're asked to use it, then change the basename accordingly
+            if(NOT ${_rel_PATH} STREQUAL "")
+                set(_basename ${_rel_PATH}/${_basename})
+            endif()
 
-       get_filename_component(_basename ${_tmp_FILE} NAME_WE)
-       # If we had a relative path and we're asked to use it, then change the basename accordingly
-       if(NOT ${_rel_PATH} STREQUAL "")
-           set(_basename ${_rel_PATH}/${_basename})
-       endif(NOT ${_rel_PATH} STREQUAL "")
+            file(READ ${_tmp_FILE} _contents)
+            string(REGEX REPLACE "^(.*\n)?File=([^\n]+kcfg).*\n.*$" "\\2"  _kcfg_FILENAME "${_contents}")
+            set(_src_FILE    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
+            set(_header_FILE ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
+            set(_moc_FILE    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
+            set(_kcfg_FILE   ${_abs_PATH}/${_kcfg_FILENAME})
+            # Maybe the .kcfg is a generated file?
+            if(NOT EXISTS "${_kcfg_FILE}")
+                set(_kcfg_FILE   ${CMAKE_CURRENT_BINARY_DIR}/${_kcfg_FILENAME})
+            endif()
+            if(NOT EXISTS "${_kcfg_FILE}")
+                message(ERROR "${_kcfg_FILENAME} not found; tried in ${_abs_PATH} and ${CMAKE_CURRENT_BINARY_DIR}")
+            endif()
 
-       file(READ ${_tmp_FILE} _contents)
-       string(REGEX REPLACE "^(.*\n)?File=([^\n]+kcfg).*\n.*$" "\\2"  _kcfg_FILENAME "${_contents}")
-       set(_src_FILE    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
-       set(_header_FILE ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
-       set(_moc_FILE    ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc)
-       set(_kcfg_FILE   ${_abs_PATH}/${_kcfg_FILENAME})
-       # Maybe the .kcfg is a generated file?
-       if(NOT EXISTS "${_kcfg_FILE}")
-           set(_kcfg_FILE   ${CMAKE_CURRENT_BINARY_DIR}/${_kcfg_FILENAME})
-       endif(NOT EXISTS "${_kcfg_FILE}")
-       if(NOT EXISTS "${_kcfg_FILE}")
-           message(ERROR "${_kcfg_FILENAME} not found; tried in ${_abs_PATH} and ${CMAKE_CURRENT_BINARY_DIR}")
-       endif(NOT EXISTS "${_kcfg_FILE}")
+            # make sure the directory exist in the build directory
+            if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH}")
+                file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH})
+            endif()
 
-       # make sure the directory exist in the build directory
-       if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH}")
-           file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH})
-       endif(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH}")
+            # if (CMAKE_CROSSCOMPILING)
+            #     set(IMPORT_KCONFIG_COMPILER_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportKConfigCompilerExecutable.cmake"
+            #         CACHE FILEPATH "Point it to the export file of kconfig_compiler from a native build")
+            #     include(${IMPORT_KCONFIG_COMPILER_EXECUTABLE})
+            #     set(KDE4_KCFGC_EXECUTABLE kconfig_compiler)
+            # endif()
 
-#       if (CMAKE_CROSSCOMPILING)
-#           set(IMPORT_KCONFIG_COMPILER_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportKConfigCompilerExecutable.cmake" CACHE FILEPATH "Point it to the export file of kconfig_compiler from a native build")
-#           include(${IMPORT_KCONFIG_COMPILER_EXECUTABLE})
-#           set(KDE4_KCFGC_EXECUTABLE kconfig_compiler)
-#       endif (CMAKE_CROSSCOMPILING)
+            # the command for creating the source file from the kcfg file
+            add_custom_command(OUTPUT ${_header_FILE} ${_src_FILE}
+                COMMAND ${KDE4_KCFGC_EXECUTABLE}
+                ARGS ${_kcfg_FILE} ${_tmp_FILE} -d ${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH}
+                MAIN_DEPENDENCY ${_tmp_FILE}
+                DEPENDS ${_kcfg_FILE}
+            )
 
-       # the command for creating the source file from the kcfg file
-       add_custom_command(OUTPUT ${_header_FILE} ${_src_FILE}
-          COMMAND ${KDE4_KCFGC_EXECUTABLE}
-          ARGS ${_kcfg_FILE} ${_tmp_FILE} -d ${CMAKE_CURRENT_BINARY_DIR}/${_rel_PATH}
-          MAIN_DEPENDENCY ${_tmp_FILE}
-          DEPENDS ${_kcfg_FILE}
-       )
+            if(_kcfg_generatemoc)
+                qt4_generate_moc(${_header_FILE} ${_moc_FILE} )
+                set_source_files_properties(${_src_FILE} PROPERTIES SKIP_AUTOMOC TRUE)
+                list(APPEND ${_sources} ${_moc_FILE})
+            endif()
 
-       if(_kcfg_generatemoc)
-         qt4_generate_moc(${_header_FILE} ${_moc_FILE} )
-         set_source_files_properties(${_src_FILE} PROPERTIES SKIP_AUTOMOC TRUE)  # don't run automoc on this file
-         list(APPEND ${_sources} ${_moc_FILE})
-       endif(_kcfg_generatemoc)
-
-       list(APPEND ${_sources} ${_src_FILE} ${_header_FILE})
-     endif(NOT ${_current_FILE} STREQUAL "GENERATE_MOC" AND NOT ${_current_FILE} STREQUAL "USE_RELATIVE_PATH")
-   endforeach (_current_FILE)
-
-endmacro (KDE4_ADD_KCFG_FILES)
+            list(APPEND ${_sources} ${_src_FILE} ${_header_FILE})
+        endif(NOT ${_current_FILE} STREQUAL "GENERATE_MOC" AND NOT ${_current_FILE} STREQUAL "USE_RELATIVE_PATH")
+    endforeach (_current_FILE)
+endmacro(KDE4_ADD_KCFG_FILES)
 
 
-get_filename_component(KDE4_MODULE_DIR  ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
+get_filename_component(KDE4_MODULE_DIR ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
 
-macro (KDE4_CREATE_MANPAGE _docbook _section)
-   get_filename_component(_input ${_docbook} ABSOLUTE)
-   get_filename_component(_base ${_input} NAME)
+macro(KDE4_CREATE_MANPAGE _docbook _section)
+    get_filename_component(_input ${_docbook} ABSOLUTE)
+    get_filename_component(_base ${_input} NAME)
 
-   string(REGEX REPLACE "\\.${_section}\\.docbook$" "" _base ${_base})
+    string(REGEX REPLACE "\\.${_section}\\.docbook$" "" _base ${_base})
 
-   set(_doc ${CMAKE_CURRENT_BINARY_DIR}/${_base}.${_section})
-   # sometimes we have "man-" prepended
-   string(REGEX REPLACE "/man-" "/" _outdoc ${_doc})
+    set(_doc ${CMAKE_CURRENT_BINARY_DIR}/${_base}.${_section})
+    # sometimes we have "man-" prepended
+    string(REGEX REPLACE "/man-" "/" _outdoc ${_doc})
 
-   add_custom_command(OUTPUT ${_outdoc}
-      COMMAND xsltproc ${_input}
-      DEPENDS ${_input} xsltproc
-   )
-   get_filename_component(_targ ${CMAKE_CURRENT_SOURCE_DIR} NAME)
-   set(_targ "${_targ}-manpage-${_base}")
-   add_custom_target(${_targ} ALL DEPENDS "${_outdoc}")
+    add_custom_command(OUTPUT ${_outdoc}
+        COMMAND xsltproc ${_input}
+        DEPENDS ${_input} xsltproc
+    )
+    get_filename_component(_targ ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+    set(_targ "${_targ}-manpage-${_base}")
+    add_custom_target(${_targ} ALL DEPENDS "${_outdoc}")
 
-   set(_args ${ARGN})
+    set(_args ${ARGN})
 
-   set(_installDest)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-         list(GET _args 1 _installDest )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-   endif(_args)
+    set(_installDest)
+    if(_args)
+        list(GET _args 0 _tmp)
+        if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+            list(GET _args 1 _installDest )
+            list(REMOVE_AT _args 0 1)
+        endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+    endif(_args)
 
-   get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "SUBDIR")
-         list(GET _args 1 dirname )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "SUBDIR")
-   endif(_args)
+    get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
+    if(_args)
+        list(GET _args 0 _tmp)
+        if("${_tmp}" STREQUAL "SUBDIR")
+            list(GET _args 1 dirname )
+            list(REMOVE_AT _args 0 1)
+        endif("${_tmp}" STREQUAL "SUBDIR")
+    endif(_args)
 
-   if(_installDest)
-      install(FILES ${_outdoc} DESTINATION ${_installDest}/man${_section})
-   endif(_installDest)
-endmacro (KDE4_CREATE_MANPAGE)
+    if(_installDest)
+        install(FILES ${_outdoc} DESTINATION ${_installDest}/man${_section})
+    endif(_installDest)
+endmacro(KDE4_CREATE_MANPAGE)
 
 
 # a "map" of short type names to the directories
@@ -174,14 +173,19 @@ set(_KDE4_ICON_THEME_hi "hicolor")
 
 # only used internally by KDE4_INSTALL_ICONS
 macro(_KDE4_ADD_ICON_INSTALL_RULE _install_SCRIPT _install_PATH _group _orig_NAME _install_NAME _l10n_SUBDIR)
-    # if the string doesn't match the pattern, the result is the full string, so all three have the same content
+    # if the string doesn't match the pattern, the result is the full string,
+    # so all three have the same content
     if(NOT ${_group} STREQUAL ${_install_NAME})
         set(_icon_GROUP  ${_KDE4_ICON_GROUP_${_group}})
         if(NOT _icon_GROUP)
             set(_icon_GROUP "actions")
         endif()
         # message(STATUS "icon: ${_current_ICON} size: ${_size} group: ${_group} name: ${_name} l10n: ${_l10n_SUBDIR}")
-        install(FILES ${_orig_NAME} DESTINATION ${_install_PATH}/${_icon_GROUP}/${_l10n_SUBDIR}/ RENAME ${_install_NAME} )
+        install(
+            FILES ${_orig_NAME}
+            DESTINATION ${_install_PATH}/${_icon_GROUP}/${_l10n_SUBDIR}/
+            RENAME ${_install_NAME}
+        )
     endif()
 endmacro(_KDE4_ADD_ICON_INSTALL_RULE)
 
@@ -198,8 +202,12 @@ macro(KDE4_INSTALL_ICONS _defaultpath )
     # first the png icons
     file(GLOB _icons *.png)
     foreach(_current_ICON ${_icons} )
-        # since CMake 2.6 regex matches are stored in special variables CMAKE_MATCH_x, if it didn't match, they are empty
-        string(REGEX MATCH "^.*/([a-zA-Z]+)([0-9]+)\\-([a-z]+)\\-(.+\\.png)$" _dummy  "${_current_ICON}")
+        # since CMake 2.6 regex matches are stored in special variables
+        # CMAKE_MATCH_x, if it didn't match, they are empty
+        string(
+            REGEX MATCH "^.*/([a-zA-Z]+)([0-9]+)\\-([a-z]+)\\-(.+\\.png)$"
+            _dummy  "${_current_ICON}"
+        )
         set(_type  "${CMAKE_MATCH_1}")
         set(_size  "${CMAKE_MATCH_2}")
         set(_group "${CMAKE_MATCH_3}")
@@ -218,8 +226,10 @@ macro(KDE4_INSTALL_ICONS _defaultpath )
     # mng icons
     file(GLOB _icons *.mng)
     foreach(_current_ICON ${_icons} )
-        # since CMake 2.6 regex matches are stored in special variables CMAKE_MATCH_x, if it didn't match, they are empty
-        string(REGEX MATCH "^.*/([a-zA-Z]+)([0-9]+)\\-([a-z]+)\\-(.+\\.mng)$" _dummy  "${_current_ICON}")
+        string(
+            REGEX MATCH "^.*/([a-zA-Z]+)([0-9]+)\\-([a-z]+)\\-(.+\\.mng)$"
+            _dummy  "${_current_ICON}"
+        )
         set(_type  "${CMAKE_MATCH_1}")
         set(_size  "${CMAKE_MATCH_2}")
         set(_group "${CMAKE_MATCH_3}")
@@ -238,8 +248,10 @@ macro(KDE4_INSTALL_ICONS _defaultpath )
     # and now the svg icons
     file(GLOB _icons *.svgz)
     foreach(_current_ICON ${_icons})
-        # since CMake 2.6 regex matches are stored in special variables CMAKE_MATCH_x, if it didn't match, they are empty
-        string(REGEX MATCH "^.*/([a-zA-Z]+)sc\\-([a-z]+)\\-(.+\\.svgz)$" _dummy "${_current_ICON}")
+        string(
+            REGEX MATCH "^.*/([a-zA-Z]+)sc\\-([a-z]+)\\-(.+\\.svgz)$"
+            _dummy "${_current_ICON}"
+        )
         set(_type  "${CMAKE_MATCH_1}")
         set(_group "${CMAKE_MATCH_2}")
         set(_name  "${CMAKE_MATCH_3}")
@@ -295,7 +307,10 @@ macro(KDE4_ADD_TEST _targetName)
     string(REPLACE "test" "" _testName ${_targetName})
     add_test(NAME ${_testName} COMMAND ${_targetName})
 
-    target_compile_definitions(${_targetName} PRIVATE -DKDESRCDIR="${CMAKE_CURRENT_SOURCE_DIR}/")
+    target_compile_definitions(
+        ${_targetName} PRIVATE
+        -DKDESRCDIR="${CMAKE_CURRENT_SOURCE_DIR}/"
+    )
 endmacro(KDE4_ADD_TEST)
 
 macro(KDE4_ADD_WIDGET_FILES _sources)
@@ -320,12 +335,16 @@ macro(KDE4_ADD_WIDGET_FILES _sources)
 endmacro(KDE4_ADD_WIDGET_FILES)
 
 
-# This macro adds the needed files for an helper executable meant to be used by applications using KAuth.
-# It accepts the helper target, the helper ID (the DBUS name) and the user under which the helper will run on.
-# This macro takes care of generate the needed files, and install them in the right location. This boils down
-# to a DBus policy to let the helper register on the system bus, and a service file for letting the helper
-# being automatically activated by the system bus.
-# *WARNING* You have to install the helper in ${LIBEXEC_INSTALL_DIR} to make sure everything will work.
+# This macro adds the needed files for an helper executable meant to be used by
+# applications using KAuth. It accepts the helper target, the helper ID (the
+# DBUS name) and the user under which the helper will run on. The macro also
+# takes care of generate the needed files, and install them in the right
+# location. This boils down to a DBus policy to let the helper register on the
+# system bus, and a service file for letting the helper being automatically
+# activated by the system bus.
+#
+# *WARNING* You have to install the helper in ${LIBEXEC_INSTALL_DIR} to make
+# sure everything will work.
 function(KDE4_INSTALL_AUTH_HELPER_FILES HELPER_TARGET HELPER_ID HELPER_USER)
     if (_kdeBootStrapping)
         set(_stubFilesDir ${CMAKE_SOURCE_DIR}/kdecore/auth/backends/dbus/)
