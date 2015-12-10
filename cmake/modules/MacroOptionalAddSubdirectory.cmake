fluxer/kdelibs

commit 824d6f119581afce4044f0a69a1398b676a643dc

cmake: make it possible to automatically skip optional directories
Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

cmake/modules/MacroOptionalAddSubdirectory.cmake
@@ -24,18 +24,23 @@
 
 MACRO (MACRO_OPTIONAL_ADD_SUBDIRECTORY _dir )
    GET_FILENAME_COMPONENT(_fullPath ${_dir} ABSOLUTE)
-   IF(EXISTS ${_fullPath}/CMakeLists.txt)
+   IF(EXISTS ${_fullPath}/CMakeLists.txt AND EXISTS ${_fullPath}/.skipdir)
+      FILE(READ ${_fullPath}/.skipdir _message)
+      MESSAGE(WARNING "${_message}")
+   ELSEIF(EXISTS ${_fullPath}/CMakeLists.txt)
       IF(DISABLE_ALL_OPTIONAL_SUBDIRECTORIES)
          SET(_DEFAULT_OPTION_VALUE FALSE)
-      ELSE(DISABLE_ALL_OPTIONAL_SUBDIRECTORIES)
+      ELSE()
          SET(_DEFAULT_OPTION_VALUE TRUE)
-      ENDIF(DISABLE_ALL_OPTIONAL_SUBDIRECTORIES)
+      ENDIF()
+
       IF(DISABLE_ALL_OPTIONAL_SUBDIRS  AND NOT DEFINED  BUILD_${_dir})
          SET(_DEFAULT_OPTION_VALUE FALSE)
-      ENDIF(DISABLE_ALL_OPTIONAL_SUBDIRS  AND NOT DEFINED  BUILD_${_dir})
+      ENDIF()
+
       OPTION(BUILD_${_dir} "Build directory ${_dir}" ${_DEFAULT_OPTION_VALUE})
       IF(BUILD_${_dir})
          ADD_SUBDIRECTORY(${_dir})
-      ENDIF(BUILD_${_dir})
+      ENDIF()
    ENDIF(EXISTS ${_fullPath}/CMakeLists.txt)
 ENDMACRO (MACRO_OPTIONAL_ADD_SUBDIRECTORY)

commit b38566b884cecaa309dca0c0b5461ac87a775aab

cmake: fix if/endif tag mismatch, regression since 824d6f11
Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

cmake/modules/MacroOptionalAddSubdirectory.cmake
@@ -42,5 +42,5 @@ MACRO (MACRO_OPTIONAL_ADD_SUBDIRECTORY _dir )
       IF(BUILD_${_dir})
          ADD_SUBDIRECTORY(${_dir})
       ENDIF()
-   ENDIF(EXISTS ${_fullPath}/CMakeLists.txt)
+   ENDIF()
 ENDMACRO (MACRO_OPTIONAL_ADD_SUBDIRECTORY)