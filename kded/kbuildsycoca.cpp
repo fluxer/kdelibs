8e30a7310277ad5b43657477b59d09d37fa97f9f
Ivailo Monev <xakepa10@gmail.com>
2016-01-25 21:04:51 +0200

    kded: set stream version to 4.0
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kded/kbuildsycoca.cpp
+++ kded/kbuildsycoca.cpp
@@ -406,7 +406,7 @@ bool KBuildSycoca::recreate()
   }
 
   QDataStream* str = new QDataStream(&database);
-  str->setVersion(QDataStream::Qt_3_1);
+  str->setVersion(QDataStream::Qt_4_0);
 
   kDebug(7021).nospace() << "Recreating ksycoca file (" << path << ", version " << KSycoca::version() << ")";
 
@@ -449,7 +449,7 @@ bool KBuildSycoca::recreate()
     QFile ksycocastamp(stamppath);
     ksycocastamp.open( QIODevice::WriteOnly );
     QDataStream str( &ksycocastamp );
-    str.setVersion(QDataStream::Qt_3_1);
+    str.setVersion(QDataStream::Qt_4_0);
     str << newTimestamp;
     str << existingResourceDirs();
     if (g_vfolder)
@@ -718,7 +718,7 @@ int main(int argc, char **argv)
        if( ksycocastamp.open( QIODevice::ReadOnly ))
        {
            QDataStream str( &ksycocastamp );
-           str.setVersion(QDataStream::Qt_3_1);
+           str.setVersion(QDataStream::Qt_4_0);
 
            if (!str.atEnd())
                str >> filestamp;
