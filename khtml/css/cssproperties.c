fluxer/kdelibs

commit 2f72cbbdbea50ad88bf214b672923b5b2969654a

khtml: use QImageReader explicitly
this allows to use char* instead of uchar* to avoid some casts,
also incremental requests are no more and the data is handled in
one go to avoid a lot of small memory allocations and extra array
for each image. it does perform slitely worse but with this a lot
more image formats can be handled with higher quality (slitely)
and even more correct, some images previously did not show
at all because of the broken image handlers.

Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

khtml/imload/image.cpp
@@ -102,7 +102,7 @@ void Image::loadError()
     owner->imageError(this);
 }
 
-bool Image::processData(uchar* data, int length)
+bool Image::processData(char* data, int length)
 {
     if (inError)
         return false;
@@ -117,71 +117,28 @@ bool Image::processData(uchar* data, int length)
         }
         else
         {
-            //need to to do auto detection... so append all the data into a buffer
-            int oldSize = bufferPreDetect.size();
-            bufferPreDetect.resize(oldSize + length);
-            memcpy(bufferPreDetect.data() + oldSize, data, length);
-
             //Attempt to create a loader
-            loader = ImageManager::loaderDatabase()->loaderFor(bufferPreDetect);
+            loader = ImageManager::loaderFor(QByteArray(data));
 
             //if can't, return...
             if (!loader)
             {
-                //if there is more than 4K of data,
-                //and we see no use for it, abort.
-                if (bufferPreDetect.size() > 4096)
-                {
-                    loadError();
-                    return false;
-                }
-                return true;
+                loadError();
+                return false;
             }
 
             loader->setImage(this);
-
-            //All the data is now in the buffer
-            length = 0;
         }
     }
 
-    int pos = 0, stat = 0;
-
-    //If we got this far, we have the loader.
-    //just feed it any buffered data, and the new data.
-    if (!bufferPreDetect.isEmpty())
-    {
-        do
-        {
-            stat = loader->processData(reinterpret_cast<uchar*>(bufferPreDetect.data() + pos),
-                                           bufferPreDetect.size() - pos);
-            if (stat == bufferPreDetect.size() - pos)
-                break;
-
-            pos += stat;
-        }
-        while (stat > 0);
-        bufferPreDetect.resize(0);
-    }
-
-    if (length) //if there is something we did not feed from the buffer already..
-    {
-        pos = 0;
-        do
-        {
-            stat = loader->processData(data + pos, length - pos);
-
-            if (stat == length - pos)
-                break;
-
-            pos  += stat;
-        }
-        while (stat > 0);
-    }
+    int stat = loader->processData(data, length);
 
     //If we just finished decoding...
     if (stat == ImageLoader::Done)
     {
+        if (original && original->animProvider)
+            original->animProvider->setShowAnimations(animationAdvice);
+
         fullyDecoded = true;
         owner->imageDone(this);
         return false;
@@ -195,40 +152,6 @@ bool Image::processData(uchar* data, int length)
 
     return true; //Need more stuff
 }
-
-void Image::processEOF()
-{
-    if (inError) //Input error already - nothing to do
-        return;
-
-    //If no loader detected, and we're at EOF, it's an error
-    if (!loader)
-    {
-        loadError();
-        return;
-    }
-
-    //Otherwise, simply tell the loader, and check whether we decoded all right
-    bool decodedOK = loader->processEOF() == ImageLoader::Done;
-
-    //... and get rid of it
-    delete loader;
-    loader = 0;
-
-    if (!decodedOK)
-    {
-        loadError();
-    }
-    else
-    {
-        if (original && original->animProvider)
-            original->animProvider->setShowAnimations(animationAdvice);
-
-        fullyDecoded = true;
-        owner->imageDone(this);
-    }
-}
-
 
 void Image::notifyImageInfo(int _width, int _height)
 {

khtml/imload/image.h
@@ -44,8 +44,7 @@ class ImageLoader;
 class PixmapPlane;
 
 /**
- An image represents a static picture or an animation, that
- may be incrementally loaded.
+ An image represents a static picture or an animation
 */
 class Image
 {
@@ -61,13 +60,7 @@ public:
      Provides new data for decoding. The method will return false if there is no longer any use to feeding it more data 
      (i.e. if the image is complete, or broken, etc.); however, it is safe to do so.
     */
-    bool processData(uchar* data, int length);
-    
-    /**
-     Notifies the image that the data source is exhausted, in case it cares. This should be called at the 
-     end of the data stream in order for non-incremental decoders to work
-    */
-    void processEOF();
+    bool processData(char* data, int length);
     
     /**
      Cleans up
@@ -168,7 +161,6 @@ protected:
     void notifyFrameChange();
 
     //Loader stuff.
-    QByteArray bufferPreDetect;
     ImageLoader* loader;
     PixmapPlane* loaderPlane;
     unsigned int loaderScanline;

khtml/imload/imageloader.h
@@ -129,19 +129,7 @@ public:
      Decodes a portion of the image, and returns the appropriate 
      status, or the number of bytes read
     */
-    virtual int processData(uchar* data, int length) = 0;
-
-    /**
-     This method is called to notify the decoder that the input is done, if the decoder 
-     has not already indicated some sort of completion on the stream; this is intended 
-     mostly for non-incremental decoders. It should return Done if all is OK. Note that 
-     the "if" above should mean that the non-incremental decoders should just return 
-     the bytes read in processData
-    */
-    virtual int processEOF()
-    {
-        return Done; //### Probably should be Error if notifyImageInfo has not been called
-    }
+    virtual int processData(char* data, int length) = 0;
 };
 
 }

khtml/imload/imagemanager.cpp
@@ -22,19 +22,22 @@
 
 */
 
-
 #include "imagemanager.h"
-#include "decoders/jpegloader.h"
-#include "decoders/pngloader.h"
-#include "decoders/gifloader.h"
-#include "decoders/qimageioloader.h"
+
+#include <QBuffer>
+#include <QByteArray>
+#include <QStringList>
+#include <QImageReader>
+
+#include "kservice.h"
+#include "kservicetypetrader.h"
+#include "kdebug.h"
 
 namespace khtmlImLoad {
 
 TileCache*      ImageManager::imgCache   = 0;
 TileCache*      ImageManager::pixCache   = 0;
 Updater*        ImageManager::theUpdater = 0;
-LoaderDatabase* ImageManager::loaderDB   = 0;
 QPixmap*        ImageManager::emptyPix   = 0;
 AnimTimer*      ImageManager::anmTimer   = 0;
 
@@ -52,14 +55,6 @@ unsigned int ImageManager::imageCacheSize()
 unsigned int ImageManager::pixmapCacheSize()
 {
     return 64*64;
-}
-
-void ImageManager::initLoaders()
-{
-    loaderDB->registerLoaderProvider(new JPEGLoaderProvider);
-    loaderDB->registerLoaderProvider(new PNGLoaderProvider);
-    loaderDB->registerLoaderProvider(new GIFLoaderProvider);
-    loaderDB->registerLoaderProvider(new QImageIOLoaderProvider);
 }
 
 bool ImageManager::isAcceptableSize(unsigned width, unsigned height)
@@ -86,6 +81,124 @@ bool ImageManager::isAcceptableScaleSize(unsigned width, unsigned height)
     // The scaling tables are at most 256KiB each. So this is all reasonable,
     // even too reasonable.
     return true;
+}
+
+class QImageIOLoader: public ImageLoader
+{
+    QBuffer buffer;
+    QImage image;
+public:
+    QImageIOLoader()
+    {
+    }
+
+    ~QImageIOLoader()
+    {
+    }
+
+    virtual int processData(char* data, int length)
+    {
+        buffer.setData(data, length);
+        buffer.open(QIODevice::ReadOnly);
+
+        QByteArray qformat = QImageReader::imageFormat(&buffer);
+        QImageReader reader(&buffer, qformat);
+
+        if (!reader.canRead()) {
+            return Error;
+        }
+
+        QSize size = reader.size();
+        if (size.isValid()) {
+            if (ImageManager::isAcceptableSize(size.width(), size.height()))
+                notifyImageInfo(size.width(), size.height());
+            else
+                return Error;
+        }
+
+        if (!reader.read(&image)) {
+            return Error;
+        }
+
+        if (!size.isValid()) {
+            // Might be too late by now..
+            if (ImageManager::isAcceptableSize(image.width(), image.height()))
+                notifyImageInfo(image.width(), image.height());
+            else
+                return Error;
+        }
+
+        ImageFormat format;
+        if (!imageFormat(image, format)) {
+            return Error;
+        }
+        notifyAppendFrame(image.width(), image.height(), format);
+
+        notifyQImage(1, &image);
+
+        return Done;
+    }
+    bool imageFormat(QImage &image, ImageFormat &format) {
+        switch(image.format()) {
+        case QImage::Format_RGB32:
+            format.type  = ImageFormat::Image_RGB_32;
+            break;
+        case QImage::Format_ARGB32:
+            format.type  = ImageFormat::Image_ARGB_32_DontPremult;
+            break;
+        case QImage::Format_ARGB32_Premultiplied:
+            format.type  = ImageFormat::Image_ARGB_32;
+            break;
+        case QImage::Format_Indexed8:
+            format.type  = ImageFormat::Image_Palette_8;
+            format.palette = image.colorTable();
+            break;
+        case QImage::Format_Mono:
+        case QImage::Format_MonoLSB:
+            image = image.convertToFormat(QImage::Format_Indexed8);
+            format.type  = ImageFormat::Image_Palette_8;
+            format.palette = image.colorTable();
+            break;
+        case QImage::Format_Invalid:
+        default:
+            // unsupported formats
+            return false;
+        }
+        return true;
+    }
+};
+
+static QStringList s_formats;
+
+const QStringList& ImageManager::supportedMimeTypes()
+{
+    if (!s_formats.isEmpty()) return s_formats;
+
+    KService::List services = KServiceTypeTrader::self()->query("QImageIOPlugins");
+    foreach(const KService::Ptr &service, services) {
+        const QStringList format = service->property("X-KDE-ImageFormat").toStringList();
+        const QString mimetype = service->property("X-KDE-MimeType").toString();
+        if (!format.isEmpty()) {
+            s_formats << format;
+            kDebug(399) << "QImageIO - Format supported: " << mimetype << endl;
+        }
+    }
+    return s_formats;
+}
+
+ImageLoader* ImageManager::loaderFor(const QByteArray& prefix)
+{
+    QByteArray pref = prefix;
+    QBuffer prefixBuffer(&pref);
+    prefixBuffer.open(QIODevice::ReadOnly);
+    QByteArray format = QImageReader::imageFormat(&prefixBuffer);
+    prefixBuffer.close();
+    if (format.isEmpty() || !supportedMimeTypes().contains(format, Qt::CaseInsensitive))
+        return 0;
+    else
+        kDebug(399) << "QImageIO - Format guessed: " << format << endl;
+
+    return new QImageIOLoader;
 }
 
 }

khtml/misc/loader.cpp
@@ -247,7 +247,7 @@ CachedCSSStyleSheet::CachedCSSStyleSheet(DocLoader* dl, const DOMString &url, KI
     // Do |not| touch the priority value unless you conducted thorough tests and
     // can back your choice with meaningful data, testing page load time and
     // time to first paint.
-    Cache::loader()->load(dl, this, false, -8);
+    Cache::loader()->load(dl, this, -8);
     m_loading = true;
 }
 
@@ -355,7 +355,7 @@ CachedScript::CachedScript(DocLoader* dl, const DOMString &url, KIO::CacheContro
     // load the file.
     // Scripts block document parsing. They are therefore second in our list of most
     // desired resources.
-    Cache::loader()->load(dl, this, false/*incremental*/, -6);
+    Cache::loader()->load(dl, this, -6);
     m_loading = true;
     m_hadError = false;
 }
@@ -736,12 +736,10 @@ void CachedImage::data ( QBuffer &_buffer, bool eof )
 #ifdef LOADER_DEBUG
     kDebug( 6060 ) << this << "buffersize =" << _buffer.buffer().size() << ", eof =" << eof << ", pos :" << _buffer.pos();
 #endif
-    i->processData((uchar*)_buffer.data().data(), _buffer.pos());
+    kDebug( 6060 ) << this << "buffersize =" << _buffer.buffer().size() << ", eof =" << eof << ", pos :" << _buffer.pos();
+    i->processData((char*)_buffer.data().data(), _buffer.pos());
 
     _buffer.close();
-
-    if (eof)
-        i->processEOF();
 }
 
 void CachedImage::finish()
@@ -769,7 +767,7 @@ CachedSound::CachedSound(DocLoader* dl, const DOMString &url, KIO::CacheControl 
     : CachedObject(url, Sound, _cachePolicy, 0)
 {
     setAccept( QLatin1String("*/*") ); // should be whatever phonon would accept...
-    Cache::loader()->load(dl, this, false/*incremental*/, 2);
+    Cache::loader()->load(dl, this, 2);
     m_loading = true;
 }
 
@@ -814,7 +812,7 @@ CachedFont::CachedFont(DocLoader* dl, const DOMString &url, KIO::CacheControl _c
     // Fonts are desired early because their absence will lead to a page being rendered
     // with a default replacement, then the text being re-rendered with the new font when it arrives.
     // This can be fairly disturbing for the reader - more than missing images for instance.
-    Cache::loader()->load(dl, this, false /*incremental*/, -4);
+    Cache::loader()->load(dl, this, -4);
     m_loading = true;
 }
 
@@ -884,11 +882,10 @@ void CachedFont::error( int /*err*/, const char* /*text*/ )
 
 // ------------------------------------------------------------------------------------------
 
-Request::Request(DocLoader* dl, CachedObject *_object, bool _incremental, int _priority)
+Request::Request(DocLoader* dl, CachedObject *_object, int _priority)
 {
     object = _object;
     object->setRequest(this);
-    incremental = _incremental;
     priority = _priority;
     m_docLoader = dl;
 }
@@ -1082,7 +1079,7 @@ CachedImage *DocLoader::requestImage( const DOM::DOMString &url)
     CachedImage* i = Cache::requestObject<CachedImage, CachedObject::Image>( this, fullURL, 0);
 
     if (i && i->status() == CachedObject::Unknown && autoloadImages())
-        Cache::loader()->load(this, i, true /*incremental*/);
+        Cache::loader()->load(this, i);
 
     return i;
 }
@@ -1148,7 +1145,7 @@ void DocLoader::setAutoloadImages( bool enable )
             if ( status != CachedObject::Unknown )
                 continue;
 
-            Cache::loader()->load(this, img, true /*incremental*/);
+            Cache::loader()->load(this, img);
         }
     }
 }
@@ -1174,7 +1171,7 @@ void DocLoader::setShowAnimations( KHTMLSettings::KAnimationAdvice showAnimation
 
 Loader::Loader() : QObject()
 {
-    m_supportedImageTypes = khtmlImLoad::ImageManager::loaderDatabase()->supportedMimeTypes();
+    m_supportedImageTypes = khtmlImLoad::ImageManager::supportedMimeTypes();
 }
 
 Loader::~Loader()
@@ -1182,9 +1179,9 @@ Loader::~Loader()
     qDeleteAll(m_requestsLoading);
 }
 
-void Loader::load(DocLoader* dl, CachedObject *object, bool incremental, int priority)
+void Loader::load(DocLoader* dl, CachedObject *object, int priority)
 {
-    Request *req = new Request(dl, object, incremental, priority);
+    Request *req = new Request(dl, object, priority);
     scheduleRequest(req);
     emit requestStarted( req->m_docLoader, req->object );
 }
@@ -1324,9 +1321,6 @@ void Loader::slotData( KIO::Job*job, const QByteArray &data )
         r->m_buffer.open( QIODevice::WriteOnly );
 
     r->m_buffer.write( data.data(), data.size() );
-
-    if(r->incremental)
-        r->object->data( r->m_buffer, false );
 }
 
 int Loader::numRequests( DocLoader* dl ) const

khtml/CMakeLists.txt
@@ -13,30 +13,6 @@ if(ENABLE_TESTING)
     add_subdirectory( tests )
 endif()
 
-find_package(JPEG)
-set_package_properties(JPEG PROPERTIES
-    DESCRIPTION "JPEG image format support"
-    URL "http://www.ijg.org"
-    TYPE REQUIRED
-    PURPOSE "Required by khtml"
-)
-
-find_package(GIF)
-set_package_properties(GIF PROPERTIES
-    DESCRIPTION "GIF image format support"
-    URL "http://sourceforge.net/projects/giflib"
-    TYPE REQUIRED
-    PURPOSE "Required by khtml"
-)
-
-find_package(PNG)
-set_package_properties(PNG PROPERTIES
-    DESCRIPTION "PNG image format support"
-    URL "http://www.libpng.org/pub/png"
-    TYPE REQUIRED
-    PURPOSE "Required by khtml"
-)
-
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${KDE4_ENABLE_EXCEPTIONS}")
 
 configure_file(
@@ -418,13 +394,6 @@ set(khtmlimload_STAT_SRCS
     ${CMAKE_SOURCE_DIR}/khtml/imload/canvasimage.cpp
 )
 
-set(decoders_STAT_SRCS
-    ${CMAKE_SOURCE_DIR}/khtml/imload/decoders/jpegloader.cpp
-    ${CMAKE_SOURCE_DIR}/khtml/imload/decoders/pngloader.cpp
-    ${CMAKE_SOURCE_DIR}/khtml/imload/decoders/gifloader.cpp
-    ${CMAKE_SOURCE_DIR}/khtml/imload/decoders/qimageioloader.cpp
-)
-
 # khtml/xpath/
 set(xpath_STAT_SRCS
     ${CMAKE_SOURCE_DIR}/khtml/xpath/expression.cpp
@@ -490,8 +459,6 @@ include_directories(
     ${CMAKE_CURRENT_BINARY_DIR}/css
 )
 
-include_directories(${JPEG_INCLUDE_DIR} ${GIF_INCLUDE_DIR} ${PNG_INCLUDE_DIR} )
-
 add_subdirectory( kmultipart )
 add_subdirectory( java )
 
@@ -528,7 +495,6 @@ set(khtml_LIB_SRCS
     ${kjs_html_STAT_SRCS}
     ${khtmldom_STAT_SRCS}
     ${khtmlimload_STAT_SRCS}
-    ${decoders_STAT_SRCS}
     # for WebCore compatibility
     ${khtmlgraphics_STAT_SRCS}
     # for SVG
@@ -561,9 +527,6 @@ target_link_libraries(khtml
     ${KDE4_KPARTS_LIBS}
     ktexteditor
     kjs
-    ${JPEG_LIBRARIES}
-    ${GIF_LIBRARIES}
-    ${PNG_LIBRARIES}
     ${X11_LIBRARIES}
     kparts
     kio

khtml/html/html_objectimpl.cpp
@@ -341,7 +341,7 @@ HTMLEmbedElementImpl* HTMLObjectBaseElementImpl::relevantEmbed()
 
 bool HTMLObjectBaseElementImpl::mimetypeHandledInternally(const QString& mime)
 {
-    QStringList supportedImageTypes = khtmlImLoad::ImageManager::loaderDatabase()->supportedMimeTypes();
+    QStringList supportedImageTypes = khtmlImLoad::ImageManager::supportedMimeTypes();
 
     bool newImageLike = supportedImageTypes.contains(mime);
 
@@ -482,7 +482,7 @@ void HTMLObjectBaseElementImpl::computeContent()
 
     // Figure out if may be we're image-like. In this case, we don't need to load anything,
     // but may need to do a detach/attach
-    QStringList supportedImageTypes = khtmlImLoad::ImageManager::loaderDatabase()->supportedMimeTypes();
+    QStringList supportedImageTypes = khtmlImLoad::ImageManager::supportedMimeTypes();
 
     bool newImageLike = effectiveServiceType.startsWith(QLatin1String("image/")) && supportedImageTypes.contains(effectiveServiceType);
 

commit ac6aaa5fee0b8829d20b032fe5062a2cbcd45e7d

khtml: cleanup and fix alpha channel detection regression since 2f72cbbd
Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

khtml/css/cssproperties.c
@@ -190,9 +190,6 @@ hash_prop (register const char *str, register unsigned int len)
 
 #ifdef __GNUC__
 __inline
-#if defined __GNUC_STDC_INLINE__ || defined __GNUC_GNU_INLINE__
-__attribute__ ((__gnu_inline__))
-#endif
 #endif
 const struct css_prop *
 findProp (register const char *str, register unsigned int len)

khtml/css/cssvalues.c
@@ -175,9 +175,6 @@ hash_val (register const char *str, register unsigned int len)
 
 #ifdef __GNUC__
 __inline
-#if defined __GNUC_STDC_INLINE__ || defined __GNUC_GNU_INLINE__
-__attribute__ ((__gnu_inline__))
-#endif
 #endif
 const struct css_value *
 findValue (register const char *str, register unsigned int len)

khtml/imload/image.cpp
@@ -131,10 +131,9 @@ bool Image::processData(char* data, int length)
         }
     }
 
-    int stat = loader->processData(data, length);
-
+    const bool success = loader->processData(data, length);
     //If we just finished decoding...
-    if (stat == ImageLoader::Done)
+    if (success)
     {
         if (original && original->animProvider)
             original->animProvider->setShowAnimations(animationAdvice);
@@ -142,15 +141,11 @@ bool Image::processData(char* data, int length)
         fullyDecoded = true;
         owner->imageDone(this);
         return false;
+
     }
 
-    if (stat == ImageLoader::Error)
-    {
-        loadError();
-        return false;
-    }
-
-    return true; //Need more stuff
+    loadError();
+    return false;
 }
 
 void Image::notifyImageInfo(int _width, int _height)
@@ -404,13 +399,6 @@ QImage* Image::qimage() const
         return 0;
 
     return &static_cast<RawImagePlane*>(original->parent)->image;
-}
-
-bool Image::hasAlpha() const
-{
-    if (!original || !original->parent)
-        return false;
-    return original->parent->format.hasAlpha();
 }
 
 void Image::setShowAnimations(KHTMLSettings::KAnimationAdvice newAdvice)

khtml/imload/image.h
@@ -77,11 +77,6 @@ public:
     bool complete() const;
 
     /**
-     Returns true if the image may have an alpha channel
-    */
-    bool hasAlpha() const;
-
-    /**
      Returns the image of basic content. Should be treated as READ ONLY.
      (but see CanvasImage)
     */

khtml/imload/imageformat.h
@@ -90,11 +90,6 @@ struct ImageFormat
         return toRet;
     }
 
-    bool hasAlpha() const
-    {
-        return  (type == Image_ARGB_32 || type == Image_ARGB_32_DontPremult);
-    }
-
     mutable QVector<QRgb> palette;
 
     //A helper for setting up a format descriptor for 8-bit grayscale

khtml/imload/imageloader.h
@@ -118,18 +118,12 @@ public:
     
     virtual ~ImageLoader()
     {}
-    
-    enum Status
-    {
-        Done  = -2,
-        Error = -1
-    };
-    
+
     /**
-     Decodes a portion of the image, and returns the appropriate 
-     status, or the number of bytes read
+     Decodes the image, and returns the appropriate status,
+     or the number of bytes read
     */
-    virtual int processData(char* data, int length) = 0;
+    virtual bool processData(char* data, int length) = 0;
 };
 
 }

khtml/imload/imagemanager.cpp
@@ -96,7 +96,7 @@ public:
     {
     }
 
-    virtual int processData(char* data, int length)
+    virtual bool processData(char* data, int length)
     {
         buffer.setData(data, length);
         buffer.open(QIODevice::ReadOnly);
@@ -105,7 +105,7 @@ public:
         QImageReader reader(&buffer, qformat);
 
         if (!reader.canRead()) {
-            return Error;
+            return false;
         }
 
         QSize size = reader.size();
@@ -113,11 +113,11 @@ public:
             if (ImageManager::isAcceptableSize(size.width(), size.height()))
                 notifyImageInfo(size.width(), size.height());
             else
-                return Error;
+                return false;
         }
 
         if (!reader.read(&image)) {
-            return Error;
+            return false;
         }
 
         if (!size.isValid()) {
@@ -125,18 +125,18 @@ public:
             if (ImageManager::isAcceptableSize(image.width(), image.height()))
                 notifyImageInfo(image.width(), image.height());
             else
-                return Error;
+                return false;
         }
 
         ImageFormat format;
         if (!imageFormat(image, format)) {
-            return Error;
+            return false;
         }
         notifyAppendFrame(image.width(), image.height(), format);
 
         notifyQImage(1, &image);
 
-        return Done;
+        return true;
     }
     bool imageFormat(QImage &image, ImageFormat &format) {
         switch(image.format()) {

khtml/misc/loader.cpp
@@ -616,7 +616,7 @@ QPixmap CachedImage::pixmap( ) const
     int w = i->size().width();
     int h = i->size().height();
 
-    if (i->hasAlpha() && QApplication::desktop()->paintEngine() &&
+    if (i->qimage()->hasAlphaChannel() && QApplication::desktop()->paintEngine() &&
                         !QApplication::desktop()->paintEngine()->hasFeature(QPaintEngine::PorterDuff)) {
         QImage im(w, h, QImage::Format_ARGB32_Premultiplied);
         QPainter paint(&im);
@@ -627,7 +627,7 @@ QPixmap CachedImage::pixmap( ) const
         return QPixmap::fromImage( im, Qt::NoOpaqueDetection );
     } else {
         QPixmap pm(w, h);
-        if (i->hasAlpha())
+        if (i->qimage()->hasAlphaChannel())
             pm.fill(Qt::transparent);
         QPainter paint(&pm);
         paint.setCompositionMode(QPainter::CompositionMode_Source);