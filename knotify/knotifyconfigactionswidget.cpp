fluxer/kdelibs

commit d398932909366c2755a880b9336bef32d7a9018a

generic: drop Phonon in favour of MPV and custom player classes
kcompactdisc was remove because there is no use for it in the
current set of applications supported, if it is needed it will
need some work to make it use KMediaPlayer or whatever.

the Plasma VideoWidget class was dropped because custom widgets
(at some point) will be dropped unless absolutely needed and
hopefully getting rid of the heavy-weight QML stuff.

the new KMediaPlayer and KMediaWidgets classes are experimental
but they have been tested and they work as they should.

Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

knotify/knotifyconfigactionswidget.cpp
@@ -20,8 +20,7 @@
 
 #include <kstandarddirs.h>
 #include <kiconloader.h>
-
-#include <phonon/mediaobject.h>
+#include <kmediaplayer.h>
 
 KNotifyConfigActionsWidget::KNotifyConfigActionsWidget( QWidget * parent )
 	: QWidget(parent)
@@ -135,8 +134,8 @@ void KNotifyConfigActionsWidget::slotPlay(  )
 		if ( search.isEmpty() )*/
 		soundURL = KUrl::fromPath( KStandardDirs::locate( "sound", soundString ) );
 	}
-	Phonon::MediaObject* media = Phonon::createPlayer( Phonon::NotificationCategory, soundURL );
-	media->play();
+	KAudioPlayer* media = new KAudioPlayer(this);
+	media->load(soundURL.url());
 	connect(media, SIGNAL(finished()), media, SLOT(deleteLater()));
 }
 

commit 65135d67afc0f810eab8adc217bcc2bf23dfe057

knotify: fix sound notification regression since d3989329
Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

knotify/knotifyconfigactionswidget.cpp
@@ -135,7 +135,7 @@ void KNotifyConfigActionsWidget::slotPlay(  )
 		soundURL = KUrl::fromPath( KStandardDirs::locate( "sound", soundString ) );
 	}
 	KAudioPlayer* media = new KAudioPlayer(this);
-	media->load(soundURL.url());
+	media->load(soundURL.prettyUrl());
 	connect(media, SIGNAL(finished()), media, SLOT(deleteLater()));
 }
 