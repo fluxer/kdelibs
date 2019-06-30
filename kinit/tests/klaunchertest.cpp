a8bc7e0f5c8459dbcbaff067c493aa5fc2496bc0
Ivailo Monev <xakepa10@laimg.moc>
2019-06-30 11:55:57 +0000

    kinit: indentation
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- kinit/tests/klaunchertest.cpp
+++ kinit/tests/klaunchertest.cpp
@@ -38,13 +38,13 @@ int main(int argc, char *argv[])
    QByteArray dcopService;
    int pid;
    int result = KToolInvocation::startServiceByDesktopName(
-		QLatin1String("konsole"), QString(), &error, &dcopService, &pid );
+                QLatin1String("konsole"), QString(), &error, &dcopService, &pid );
 
    printf("Result = %d, error = \"%s\", dcopService = \"%s\", pid = %d\n",
       result, error.toLatin1().constData(), dcopService.data(), pid);
 
    result = KToolInvocation::startServiceByDesktopName(
-		QLatin1String("konqueror"), QString(),  &error, &dcopService, &pid );
+                QLatin1String("konqueror"), QString(),  &error, &dcopService, &pid );
 
    printf("Result = %d, error = \"%s\", dcopService = \"%s\", pid = %d\n",
       result, error.toLatin1().constData(), dcopService.data(), pid);
