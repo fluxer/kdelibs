597a84c0ef66ce1dc5a7ed716036f8ffc07fd072
Ivailo Monev <xakepa10@laimg.moc>
2020-02-02 01:48:40 +0000

    kdecore: rework KCharsetsTest test
    the static initializer was removed since Katie does not require it
    
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- kdecore/tests/kcharsetstest.cpp
+++ kdecore/tests/kcharsetstest.cpp
@@ -1,34 +1,20 @@
-
+#include <qtest_kde.h>
 #include <kcharsets.h>
-#include <kdebug.h>
-#include <QtCore/QString>
 
-#include <assert.h>
-
-int main()
+class KCharsetsTest : public QObject
 {
-    // Note: ctest sets LANG=C, while running the test directly uses your real $LANG.
-    // And since the static initializer runs before main, we cannot setenv("LANG") here,
-    // it's too late.
-    //kDebug() << "LANG=" << getenv("LANG");
-    //kDebug() << "LC_ALL=" << getenv("LC_ALL");
+    Q_OBJECT
+private Q_SLOTS:
+    void resolveEntities();
+};
 
-    // Test that toLocal8Bit works even without a QCoreApplication,
-    // thanks to the static initializer in KCatalog.
-    // Do NOT move this code to a QTestLib unit test ;-)
-    QString one = QString::fromUtf8("é");
-    QByteArray one8bit = one.toLocal8Bit();
-    QByteArray lang = qgetenv("LC_ALL");
-    if (lang.isEmpty()) {
-        lang = qgetenv("LANG");
-    }
-    if (lang.endsWith("UTF-8")) { // krazy:exclude=strings
-        kDebug() << one << one8bit;
-        Q_ASSERT(one8bit.length() == 2);
-    }
+QTEST_KDEMAIN_CORE( KCharsetsTest )
 
+void KCharsetsTest::resolveEntities()
+{
     QString input( "&lt;Hello &amp;World&gt;" );
     QString output = KCharsets::resolveEntities( input );
-    assert( output == "<Hello &World>" );
-    return 0;
+    QCOMPARE(output, QLatin1String("<Hello &World>"));
 }
+
+#include "kcharsetstest.moc"
