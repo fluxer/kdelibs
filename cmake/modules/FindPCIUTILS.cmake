eb8adea4ce26668b0160e644db3e6ad6878747b1
Ivailo Monev <xakepa10@laimg.moc>
2020-02-20 22:06:56 +0000

    cmake: rewrite PCIUTILS module
    it was causing overlinkg due to incorrect dependency additions added by the
    module because resolv library is used privately. in fact, it uses zlib and
    udev too but only privately and none of their include directories are
    needed to use libpci.
    
    since the rewrite makes use of pkg-config as primary lookup method any
    future changes to that behaviour will be handled correctly without
    overlinking.
    
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- cmake/modules/FindPCIUTILS.cmake
+++ cmake/modules/FindPCIUTILS.cmake
@@ -1,29 +1,42 @@
 # - Try to find the pciutils directory library
+#
 # Once done this will define
 #
 #  PCIUTILS_FOUND - system has PCIUtils
 #  PCIUTILS_INCLUDE_DIR - the PCIUTILS include directory
-#  PCIUTILS_LIBRARIES - The libraries needed to use PCIUtils
+#  PCIUTILS_LIBRARIES - the libraries needed to use PCIUtils
+#
+# Copyright (c) 2020, Ivailo Monev, <xakepa10@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if(PCIUTILS_INCLUDE_DIR AND PCIUTILS_LIBRARIES)
-   set(PCIUTILS_FIND_QUIETLY TRUE)
-endif(PCIUTILS_INCLUDE_DIR AND PCIUTILS_LIBRARIES)
+if(NOT WIN32)
+    include(FindPkgConfig)
+    pkg_check_modules(PC_PCIUTILS QUIET libpci)
 
-FIND_PATH(PCIUTILS_INCLUDE_DIR pci/pci.h)
+    set(PCIUTILS_INCLUDE_DIR ${PC_PCIUTILS_INCLUDE_DIRS})
+    set(PCIUTILS_LIBRARIES ${PC_PCIUTILS_LIBRARIES})
+endif()
 
-FIND_LIBRARY(PCIUTILS_LIBRARY NAMES pci)
-if(PCIUTILS_LIBRARY)
-  FIND_LIBRARY(RESOLV_LIBRARY NAMES resolv)
-  if(RESOLV_LIBRARY)
-    set(PCIUTILS_LIBRARIES ${PCIUTILS_LIBRARY} ${RESOLV_LIBRARY})
-  else(RESOLV_LIBRARY)
-    set(PCIUTILS_LIBRARIES ${PCIUTILS_LIBRARY})
-  endif(RESOLV_LIBRARY)
-endif(PCIUTILS_LIBRARY)
+set(PCIUTILS_VERSION ${PC_PCIUTILS_VERSION})
 
+if(NOT PCIUTILS_INCLUDE_DIR OR NOT PCIUTILS_LIBRARIES)
+    find_path(PCIUTILS_INCLUDE_DIR
+        NAMES pci/pci.h
+        HINTS $ENV{PCIUTILSDIR}/include
+    )
+
+    find_library(PCIUTILS_LIBRARIES
+        NAMES pci
+        HINTS $ENV{PCIUTILSDIR}/lib
+    )
+endif()
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(PCIUTILS DEFAULT_MSG PCIUTILS_LIBRARIES PCIUTILS_INCLUDE_DIR)
+find_package_handle_standard_args(PCIUTILS
+    VERSION_VAR PCIUTILS_VERSION
+    REQUIRED_VARS PCIUTILS_LIBRARIES PCIUTILS_INCLUDE_DIR
+)
 
-MARK_AS_ADVANCED(PCIUTILS_INCLUDE_DIR PCIUTILS_LIBRARIES)
-
+mark_as_advanced(PCIUTILS_INCLUDE_DIR PCIUTILS_LIBRARIES)
