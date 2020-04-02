959f5e499d3093026d50523a8e24cd3b4cb8b026
Ivailo Monev <xakepa10@laimg.moc>
2020-04-02 09:30:20 +0000

    cmake: rewrite LibGPS and Speechd modules
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- cmake/modules/FindLibGPS.cmake
+++ cmake/modules/FindLibGPS.cmake
@@ -1,16 +1,42 @@
-# LIBGPS_FOUND - system has the LIBGPS library
-# LIBGPS_INCLUDE_DIR - the LIBGPS include directory
-# LIBGPS_LIBRARIES - The libraries needed to use LIBGPS
+# - Try to find GPS library
+#
+# Once done this will define
+#
+# LIBGPS_FOUND - system has GPS library
+# LIBGPS_INCLUDE_DIR - the GPS library include directory
+# LIBGPS_LIBRARIES - the libraries needed to use GPS library
+#
+# Copyright (c) 2020, Ivailo Monev, <xakepa10@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
-  set(LIBGPS_FOUND TRUE)
-else(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
+if(NOT WIN32)
+    include(FindPkgConfig)
+    pkg_check_modules(PC_LIBGPS QUIET libgps)
 
-  find_path(LIBGPS_INCLUDE_DIR NAMES gps.h)
-  find_library(LIBGPS_LIBRARIES NAMES gps)
+    set(LIBGPS_INCLUDE_DIR ${PC_LIBGPS_INCLUDE_DIRS})
+    set(LIBGPS_LIBRARIES ${PC_LIBGPS_LIBRARIES})
+endif()
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(libgps DEFAULT_MSG LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
+set(LIBGPS_VERSION ${PC_LIBGPS_VERSION})
 
-  mark_as_advanced(LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
-endif(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
+if(NOT LIBGPS_INCLUDE_DIR OR NOT LIBGPS_LIBRARIES)
+    find_path(LIBGPS_INCLUDE_DIR
+        NAMES gps.h
+        HINTS $ENV{LIBGPSDIR}/include
+    )
+
+    find_library(LIBGPS_LIBRARIES
+        NAMES gps
+        HINTS $ENV{LIBGPSDIR}/lib
+    )
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibGPS
+    VERSION_VAR LIBGPS_VERSION
+    REQUIRED_VARS LIBGPS_LIBRARIES LIBGPS_INCLUDE_DIR
+)
+
+mark_as_advanced(LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
--- cmake/modules/FindSpeechd.cmake
+++ cmake/modules/FindSpeechd.cmake
@@ -1,17 +1,43 @@
-# find speech-dispatcher library and header if available
-# Copyright (c) 2009, Jeremy Whiting <jpwhiting@kde.org>
-# Copyright (c) 2011, Raphael Kubo da Costa <kubito@gmail.com>
-# This module defines
-#  SPEECHD_INCLUDE_DIR, where to find libspeechd.h
-#  SPEECHD_LIBRARIES, the libraries needed to link against speechd
-#  SPEECHD_FOUND, If false, speechd was not found
+# - Try to find speech-dispatcher
+#
+# Once done this will define
+#
+#  SPEECHD_FOUND - system has speech-dispatcher
+#  SPEECHD_INCLUDE_DIR - the speech-dispatcher include directory
+#  SPEECHD_LIBRARIES - the libraries needed to use speech-dispatcher
+#
+# Copyright (c) 2020, Ivailo Monev, <xakepa10@gmail.com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-find_path(SPEECHD_INCLUDE_DIR libspeechd.h PATH_SUFFIXES speech-dispatcher)
+if(NOT WIN32)
+    include(FindPkgConfig)
+    pkg_check_modules(PC_SPEECHD QUIET speech-dispatcher)
 
-find_library(SPEECHD_LIBRARIES NAMES speechd)
+    set(SPEECHD_INCLUDE_DIR ${PC_SPEECHD_INCLUDE_DIRS})
+    set(SPEECHD_LIBRARIES ${PC_SPEECHD_LIBRARIES})
+endif()
+
+set(SPEECHD_VERSION ${PC_SPEECHD_VERSION})
+
+if(NOT SPEECHD_INCLUDE_DIR OR NOT SPEECHD_LIBRARIES)
+    find_path(SPEECHD_INCLUDE_DIR
+        NAMES libspeechd.h
+        PATH_SUFFIXES speech-dispatcher
+        HINTS $ENV{SPEECHDDIR}/include
+    )
+
+    find_library(SPEECHD_LIBRARIES
+        NAMES speechd
+        HINTS $ENV{SPEECHDDIR}/lib
+    )
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Speechd REQUIRED_VARS SPEECHD_INCLUDE_DIR SPEECHD_LIBRARIES)
+find_package_handle_standard_args(Speechd
+    VERSION_VAR SPEECHD_VERSION
+    REQUIRED_VARS SPEECHD_INCLUDE_DIR SPEECHD_LIBRARIES
+)
+
+mark_as_advanced(SPEECHD_INCLUDE_DIR SPEECHD_LIBRARIES)
