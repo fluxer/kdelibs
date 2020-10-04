20eb365ce73750365dd2732a6a10713eb85d30e4
Ivailo Monev <xakepa10@gmail.com>
2020-10-04 09:26:42 +0300

    cmake: add FFmpegThumbnailer module
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ cmake/modules/FindFFmpegThumbnailer.cmake
@@ -0,0 +1,43 @@
+# - Try to find FFmpegthumbnailer library
+#
+# Once done this will define
+#
+#  FFMPEGTHUMBNAILER_FOUND - system has FFmpegthumbnailer
+#  FFMPEGTHUMBNAILER_INCLUDES - the FFmpegthumbnailer include directory
+#  FFMPEGTHUMBNAILER_LIBRARIES - the libraries needed to use FFmpegthumbnailer
+#
+# Copyright (c) 2020, Ivailo Monev, <xakepa10@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+
+if(NOT WIN32)
+    include(FindPkgConfig)
+    pkg_check_modules(PC_FFMPEGTHUMBNAILER QUIET libffmpegthumbnailer)
+
+    set(FFMPEGTHUMBNAILER_INCLUDES ${PC_FFMPEGTHUMBNAILER_INCLUDE_DIRS})
+    set(FFMPEGTHUMBNAILER_LIBRARIES ${PC_FFMPEGTHUMBNAILER_LIBRARIES})
+endif()
+
+set(FFMPEGTHUMBNAILER_VERSION ${PC_FFMPEGTHUMBNAILER_VERSION})
+
+if(NOT FFMPEGTHUMBNAILER_INCLUDES OR NOT FFMPEGTHUMBNAILER_LIBRARIES)
+    find_path(FFMPEGTHUMBNAILER_INCLUDES
+        NAMES libffmpegthumbnailer/videothumbnailer.h
+        HINTS $ENV{FFMPEGTHUMBNAILER}/include
+    )
+
+    find_library(FFMPEGTHUMBNAILER_LIBRARIES
+        NAMES ffmpegthumbnailer
+        HINTS $ENV{FFMPEGTHUMBNAILER}/lib
+    )
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(FFmpegThumbnailer
+    VERSION_VAR FFMPEGTHUMBNAILER_VERSION
+    REQUIRED_VARS FFMPEGTHUMBNAILER_LIBRARIES FFMPEGTHUMBNAILER_INCLUDES
+)
+
+mark_as_advanced(FFMPEGTHUMBNAILER_INCLUDES FFMPEGTHUMBNAILER_LIBRARIES)
