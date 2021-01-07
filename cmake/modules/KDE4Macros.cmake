5ea7027075e6ed43f692fdf9e2cde465677260d3
Ivailo Monev <xakepa10@gmail.com>
2021-01-07 14:35:28 +0200

    cmake: remove unused kde4_create_manpage() macro
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- cmake/modules/KDE4Macros.cmake
+++ cmake/modules/KDE4Macros.cmake
@@ -6,7 +6,6 @@
 # KDE4_ADD_TEST
 # KDE4_ADD_WIDGET
 # KDE4_INSTALL_ICONS
-# KDE4_CREATE_MANPAGE
 # KDE4_INSTALL_AUTH_HELPER_FILES
 # KDE4_AUTH_INSTALL_ACTIONS
 # KDE4_INSTALL_TS_FILES
@@ -14,7 +13,7 @@
 # Copyright (c) 2006-2009 Alexander Neundorf, <neundorf@kde.org>
 # Copyright (c) 2006, 2007, Laurent Montel, <montel@kde.org>
 # Copyright (c) 2007 Matthias Kretz <kretz@kde.org>
-# Copyright (c) 2015 Ivailo Monev <xakepa10@gmail.com>
+# Copyright (c) 2015-2021 Ivailo Monev <xakepa10@gmail.com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
@@ -96,52 +95,6 @@ macro(KDE4_ADD_KCFG_FILES _sources )
         endif(NOT ${_current_FILE} STREQUAL "GENERATE_MOC" AND NOT ${_current_FILE} STREQUAL "USE_RELATIVE_PATH")
     endforeach (_current_FILE)
 endmacro(KDE4_ADD_KCFG_FILES)
-
-
-get_filename_component(KDE4_MODULE_DIR ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
-
-macro(KDE4_CREATE_MANPAGE _docbook _section)
-    get_filename_component(_input ${_docbook} ABSOLUTE)
-    get_filename_component(_base ${_input} NAME)
-
-    string(REGEX REPLACE "\\.${_section}\\.docbook$" "" _base ${_base})
-
-    set(_doc ${CMAKE_CURRENT_BINARY_DIR}/${_base}.${_section})
-    # sometimes we have "man-" prepended
-    string(REGEX REPLACE "/man-" "/" _outdoc ${_doc})
-
-    add_custom_command(OUTPUT ${_outdoc}
-        COMMAND xsltproc ${_input}
-        DEPENDS ${_input} xsltproc
-    )
-    get_filename_component(_targ ${CMAKE_CURRENT_SOURCE_DIR} NAME)
-    set(_targ "${_targ}-manpage-${_base}")
-    add_custom_target(${_targ} ALL DEPENDS "${_outdoc}")
-
-    set(_args ${ARGN})
-
-    set(_installDest)
-    if(_args)
-        list(GET _args 0 _tmp)
-        if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-            list(GET _args 1 _installDest )
-            list(REMOVE_AT _args 0 1)
-        endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-    endif(_args)
-
-    get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
-    if(_args)
-        list(GET _args 0 _tmp)
-        if("${_tmp}" STREQUAL "SUBDIR")
-            list(GET _args 1 dirname )
-            list(REMOVE_AT _args 0 1)
-        endif("${_tmp}" STREQUAL "SUBDIR")
-    endif(_args)
-
-    if(_installDest)
-        install(FILES ${_outdoc} DESTINATION ${_installDest}/man${_section})
-    endif(_installDest)
-endmacro(KDE4_CREATE_MANPAGE)
 
 
 # KDE 4 / icon naming specification compatibility
