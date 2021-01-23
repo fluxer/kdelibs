c891c0cd827fcbca04d1b36208df27ad8b01bba1
Ivailo Monev <xakepa10@gmail.com>
2021-01-23 02:24:58 +0200

    kio: correct logic for terminal options
    patches from NetBSD's pkgsrc
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kio/kfile/kpropertiesdialog.cpp
+++ kio/kfile/kpropertiesdialog.cpp
@@ -3258,7 +3258,7 @@ void KDesktopPropsPlugin::slotAdvanced()
 
     if (preferredTerminal == "konsole")
     {
-        terminalCloseBool = (d->m_terminalOptionStr.contains( "--noclose" ) > 0);
+        terminalCloseBool = d->m_terminalOptionStr.contains( "--noclose" );
         w.terminalCloseCheck->setChecked(terminalCloseBool);
         d->m_terminalOptionStr.remove( "--noclose");
     }
