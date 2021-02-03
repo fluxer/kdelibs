f3af1c85c7aeb2f9dc1919925b93c4653130aa69
Ivailo Monev <xakepa10@gmail.com>
2021-02-03 08:58:51 +0200

    cmake: set CMAKE_NO_SYSTEM_FROM_IMPORTED to ON from KDE4Defaults
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- cmake/modules/KDE4Defaults.cmake
+++ cmake/modules/KDE4Defaults.cmake
@@ -4,7 +4,7 @@ set(CMAKE_AUTOMOC ON)
 # since cmake 3.0.0
 set(CMAKE_AUTOUIC ON)
 
-# Always include srcdir and builddir in include path, this saves typing
+# always include srcdir and builddir in include path, this saves typing
 # ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR} in about every subdir
 # since cmake 2.4.0
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
@@ -15,13 +15,20 @@ set(CMAKE_INCLUDE_CURRENT_DIR ON)
 # since cmake 2.4.1
 set(CMAKE_INCLUDE_DIRECTORIES_PROJECT_BEFORE ON)
 
+# do not duplicate interface includes as system from projects providing CMake
+# config in case the include directories are already included explicitly
+# (via include_directories() which always uses non-system style), reduces the
+# command-line arguments that have to be passed to the compiler and most likely
+# the build times
+set(CMAKE_NO_SYSTEM_FROM_IMPORTED ON)
+
 # define the generic version of the libraries here, this makes it easy to
 # advance it when the next KDE release comes. Use this version number for
 # libraries
 set(GENERIC_LIB_VERSION "4.20")
 set(GENERIC_LIB_SOVERSION "4")
 
-# Use this version number for libraries which are experimental
+# use this version number for libraries which are experimental
 set(KDE_NON_GENERIC_LIB_VERSION "5.16")
 set(KDE_NON_GENERIC_LIB_SOVERSION "5")
 
