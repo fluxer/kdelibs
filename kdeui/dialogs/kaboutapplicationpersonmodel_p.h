8fcac47d04a27100f67cfaf5f2ae924ace5a6094
Ivailo Monev <xakepa10@gmail.com>
2021-03-19 06:30:47 +0200

    kdeui: remove redundant forward class declarations
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdeui/dialogs/kaboutapplicationpersonmodel_p.h
+++ kdeui/dialogs/kaboutapplicationpersonmodel_p.h
@@ -27,11 +27,6 @@
 #include <QtGui/QPixmap>
 #include <QtNetwork/QNetworkReply>
 
-// Forward declarations to make Attica-related members work
-namespace Attica {
-class BaseJob;
-}
-
 namespace KDEPrivate
 {
 
