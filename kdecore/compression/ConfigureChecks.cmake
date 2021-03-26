e735f1fd503cb8858cd4eb750de026bf53eaa6bb
Ivailo Monev <xakepa10@gmail.com>
2021-03-26 21:09:16 +0200

    generic: move find_package() calls to top-level build file
    reference:
    https://github.com/fluxer/katana/issues/7
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdecore/compression/ConfigureChecks.cmake
+++ kdecore/compression/ConfigureChecks.cmake
@@ -1,17 +1,3 @@
-macro_optional_find_package(BZip2)
-set_package_properties(BZip2 PROPERTIES
-    DESCRIPTION "Support for BZip2 compressed files and data streams"
-    URL "http://www.bzip.org"
-    TYPE RECOMMENDED
-)
-
-macro_optional_find_package(LibLZMA)
-set_package_properties(LibLZMA PROPERTIES
-    DESCRIPTION "Support for xz compressed files and data streams"
-    URL "http://tukaani.org/xz/"
-    TYPE OPTIONAL
-)
-
 macro_bool_to_01(BZIP2_FOUND HAVE_BZIP2_SUPPORT)
 if(BZIP2_FOUND AND BZIP2_NEED_PREFIX)
     set(NEED_BZ2_PREFIX 1)
