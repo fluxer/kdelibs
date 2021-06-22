04b95f0de732288e93f105be38e65f4fb65b6c49
Ivailo Monev <xakepa10@gmail.com>
2021-06-22 19:09:23 +0300

    kdecore: indent and format
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdecore/io/klockfile_unix.cpp
+++ kdecore/io/klockfile_unix.cpp
@@ -118,69 +118,69 @@ public:
 KLockFile::KLockFile(const QString &file, const KComponentData &componentData)
     : d(new Private(componentData))
 {
-  d->m_fileName = file;
+    d->m_fileName = file;
 }
 
 KLockFile::~KLockFile()
 {
-  unlock();
-  delete d;
+    unlock();
+    delete d;
 }
 
 int
 KLockFile::staleTime() const
 {
-  return d->staleTime;
+    return d->staleTime;
 }
 
 
 void
 KLockFile::setStaleTime(int _staleTime)
 {
-  d->staleTime = _staleTime;
+    d->staleTime = _staleTime;
 }
 
 static bool operator==( const KDE_struct_stat &st_buf1,
             const KDE_struct_stat &st_buf2)
 {
 #define FIELD_EQ(what)       (st_buf1.what == st_buf2.what)
-  return FIELD_EQ(st_dev) && FIELD_EQ(st_ino) &&
-         FIELD_EQ(st_uid) && FIELD_EQ(st_gid) && FIELD_EQ(st_nlink);
+    return FIELD_EQ(st_dev) && FIELD_EQ(st_ino) &&
+           FIELD_EQ(st_uid) && FIELD_EQ(st_gid) && FIELD_EQ(st_nlink);
 #undef FIELD_EQ
 }
 
 static bool operator!=( const KDE_struct_stat& st_buf1,
             const KDE_struct_stat& st_buf2 )
 {
-  return !(st_buf1 == st_buf2);
+    return !(st_buf1 == st_buf2);
 }
 
 static bool testLinkCountSupport(const QByteArray &fileName)
 {
-   KDE_struct_stat st_buf;
-   int result = -1;
-   // Check if hardlinks raise the link count at all?
-   if(!::link( fileName, QByteArray(fileName+".test") )) {
-     result = KDE_lstat( fileName, &st_buf );
-     ::unlink( QByteArray(fileName+".test") );
-   }
-   return (result < 0 || ((result == 0) && (st_buf.st_nlink == 2)));
+    KDE_struct_stat st_buf;
+    int result = -1;
+    // Check if hardlinks raise the link count at all?
+    if(!::link( fileName, QByteArray(fileName+".test") )) {
+        result = KDE_lstat( fileName, &st_buf );
+        ::unlink( QByteArray(fileName+".test") );
+    }
+    return (result < 0 || ((result == 0) && (st_buf.st_nlink == 2)));
 }
 
 void KLockFile::Private::writeIntoLockFile(QFile& file, const KComponentData& componentData)
 {
-  file.setPermissions(QFile::ReadUser|QFile::WriteUser|QFile::ReadGroup|QFile::ReadOther);
+    file.setPermissions(QFile::ReadUser|QFile::WriteUser|QFile::ReadGroup|QFile::ReadOther);
 
-  m_hostname = QHostInfo::localHostName();
-  m_componentName = componentData.componentName();
+    m_hostname = QHostInfo::localHostName();
+    m_componentName = componentData.componentName();
 
-  QTextStream stream(&file);
-  m_pid = getpid();
+    QTextStream stream(&file);
+    m_pid = getpid();
 
-  stream << QString::number(m_pid) << endl
-      << m_componentName << endl
-      << m_hostname << endl;
-  stream.flush();
+    stream << QString::number(m_pid) << endl
+           << m_componentName << endl
+           << m_hostname << endl;
+    stream.flush();
 }
 
 void KLockFile::Private::readLockFile()
@@ -190,8 +190,7 @@ void KLockFile::Private::readLockFile()
     m_componentName.clear();
 
     QFile file(m_fileName);
-    if (file.open(QIODevice::ReadOnly))
-    {
+    if (file.open(QIODevice::ReadOnly)) {
         QTextStream ts(&file);
         if (!ts.atEnd())
             m_pid = ts.readLine().toInt();
@@ -204,53 +203,57 @@ void KLockFile::Private::readLockFile()
 
 KLockFile::LockResult KLockFile::Private::lockFileWithLink(KDE_struct_stat &st_buf)
 {
-  const QByteArray lockFileName = QFile::encodeName( m_fileName );
-  int result = KDE_lstat( lockFileName, &st_buf );
-  if (result == 0) {
-     return KLockFile::LockFail;
-  }
+    const QByteArray lockFileName = QFile::encodeName( m_fileName );
+    int result = KDE_lstat( lockFileName, &st_buf );
+    if (result == 0) {
+        return KLockFile::LockFail;
+    }
 
-  KTemporaryFile uniqueFile(m_componentData);
-  uniqueFile.setFileTemplate(m_fileName);
-  if (!uniqueFile.open())
-     return KLockFile::LockError;
+    KTemporaryFile uniqueFile(m_componentData);
+    uniqueFile.setFileTemplate(m_fileName);
+    if (!uniqueFile.open()) {
+        return KLockFile::LockError;
+    }
 
-  writeIntoLockFile(uniqueFile, m_componentData);
+    writeIntoLockFile(uniqueFile, m_componentData);
 
-  QByteArray uniqueName = QFile::encodeName( uniqueFile.fileName() );
+    QByteArray uniqueName = QFile::encodeName( uniqueFile.fileName() );
 
-  // Create lock file
-  result = ::link( uniqueName, lockFileName );
-  if (result != 0)
-     return KLockFile::LockError;
+    // Create lock file
+    result = ::link( uniqueName, lockFileName );
+    if (result != 0) {
+        return KLockFile::LockError;
+    }
 
-  if (!linkCountSupport)
-     return KLockFile::LockOK;
+    if (!linkCountSupport) {
+        return KLockFile::LockOK;
+    }
 
-  KDE_struct_stat st_buf2;
-  result = KDE_lstat( uniqueName, &st_buf2 );
-  if (result != 0)
-     return KLockFile::LockError;
+    KDE_struct_stat st_buf2;
+    result = KDE_lstat( uniqueName, &st_buf2 );
+    if (result != 0) {
+        return KLockFile::LockError;
+    }
 
-  result = KDE_lstat( lockFileName, &st_buf );
-  if (result != 0)
-     return KLockFile::LockError;
+    result = KDE_lstat( lockFileName, &st_buf );
+    if (result != 0) {
+        return KLockFile::LockError;
+    }
 
-  if (st_buf != st_buf2 || S_ISLNK(st_buf.st_mode) || S_ISLNK(st_buf2.st_mode))
-  {
-     // SMBFS supports hardlinks by copying the file, as a result the above test will always fail
-     // cifs increases link count artifically but the inodes are still different
-     if ((st_buf2.st_nlink > 1 ||
-         ((st_buf.st_nlink == 1) && (st_buf2.st_nlink == 1))) && (st_buf.st_ino != st_buf2.st_ino))
-     {
-        linkCountSupport = testLinkCountSupport(uniqueName);
-        if (!linkCountSupport)
-           return KLockFile::LockOK; // Link count support is missing... assume everything is OK.
-     }
-     return KLockFile::LockFail;
-  }
+    if (st_buf != st_buf2 || S_ISLNK(st_buf.st_mode) || S_ISLNK(st_buf2.st_mode)) {
+        // SMBFS supports hardlinks by copying the file, as a result the above test will always fail
+        // cifs increases link count artifically but the inodes are still different
+        if ((st_buf2.st_nlink > 1 ||
+            ((st_buf.st_nlink == 1) && (st_buf2.st_nlink == 1))) && (st_buf.st_ino != st_buf2.st_ino))
+        {
+            linkCountSupport = testLinkCountSupport(uniqueName);
+            if (!linkCountSupport)
+                return KLockFile::LockOK; // Link count support is missing... assume everything is OK.
+        }
+        return KLockFile::LockFail;
+    }
 
-  return KLockFile::LockOK;
+    return KLockFile::LockOK;
 }
 
 bool KLockFile::Private::isNfs() const
@@ -298,15 +301,17 @@ KLockFile::LockResult KLockFile::Private::lockFileOExcl(KDE_struct_stat &st_buf)
 
     // stat to get the modification time
     const int result = KDE_lstat(QFile::encodeName(m_fileName), &st_buf);
-    if (result != 0)
+    if (result != 0) {
         return KLockFile::LockError;
+    }
     return KLockFile::LockOK;
 }
 
 KLockFile::LockResult KLockFile::Private::deleteStaleLock()
 {
-    if (isNfs())
+    if (isNfs()) {
         return deleteStaleLockWithLink();
+    }
 
     // I see no way to prevent the race condition here, where we could
     // delete a new lock file that another process just got after we
@@ -333,170 +338,162 @@ KLockFile::LockResult KLockFile::Private::deleteStaleLockWithLink()
     const QByteArray tmpFile = QFile::encodeName(ktmpFile->fileName());
     delete ktmpFile;
 
-   // link to lock file
-   if (::link(lckFile, tmpFile) != 0)
-      return KLockFile::LockFail; // Try again later
+    // link to lock file
+    if (::link(lckFile, tmpFile) != 0) {
+        return KLockFile::LockFail; // Try again later
+    }
 
-   // check if link count increased with exactly one
-   // and if the lock file still matches
-   KDE_struct_stat st_buf1;
-   KDE_struct_stat st_buf2;
-   memcpy(&st_buf1, &statBuf, sizeof(KDE_struct_stat));
-   st_buf1.st_nlink++;
-   if ((KDE_lstat(tmpFile, &st_buf2) == 0) && st_buf1 == st_buf2)
-   {
-      if ((KDE_lstat(lckFile, &st_buf2) == 0) && st_buf1 == st_buf2)
-      {
-         // - - if yes, delete lock file, delete temp file, retry lock
-         kWarning() << "Deleting stale lockfile" << lckFile.data();
-         ::unlink(lckFile);
-         ::unlink(tmpFile);
-         return KLockFile::LockOK;
-      }
-   }
+    // check if link count increased with exactly one
+    // and if the lock file still matches
+    KDE_struct_stat st_buf1;
+    KDE_struct_stat st_buf2;
+    memcpy(&st_buf1, &statBuf, sizeof(KDE_struct_stat));
+    st_buf1.st_nlink++;
+    if ((KDE_lstat(tmpFile, &st_buf2) == 0) && st_buf1 == st_buf2) {
+        if ((KDE_lstat(lckFile, &st_buf2) == 0) && st_buf1 == st_buf2) {
+            // - - if yes, delete lock file, delete temp file, retry lock
+            kWarning() << "Deleting stale lockfile" << lckFile.data();
+            ::unlink(lckFile);
+            ::unlink(tmpFile);
+            return KLockFile::LockOK;
+        }
+    }
 
-   // SMBFS supports hardlinks by copying the file, as a result the above test will always fail
-   if (linkCountSupport)
-   {
-      linkCountSupport = testLinkCountSupport(tmpFile);
-   }
+    // SMBFS supports hardlinks by copying the file, as a result the above test will always fail
+    if (linkCountSupport) {
+        linkCountSupport = testLinkCountSupport(tmpFile);
+    }
 
-   if (!linkCountSupport)
-   {
-      // Without support for link counts we will have a little race condition
-      kWarning() << "Deleting stale lockfile" << lckFile.data();
-      ::unlink(tmpFile);
-      if (::unlink(lckFile) < 0) {
-          kWarning() << "Problem deleting stale lockfile" << lckFile.data() << ": " << strerror(errno);
-          return KLockFile::LockFail;
-      }
-      return KLockFile::LockOK;
-   }
+    if (!linkCountSupport) {
+        // Without support for link counts we will have a little race condition
+        kWarning() << "Deleting stale lockfile" << lckFile.data();
+        ::unlink(tmpFile);
+        if (::unlink(lckFile) < 0) {
+            kWarning() << "Problem deleting stale lockfile" << lckFile.data() << ": " << strerror(errno);
+            return KLockFile::LockFail;
+        }
+        return KLockFile::LockOK;
+    }
 
-   // Failed to delete stale lock file
-   kWarning() << "Problem deleting stale lockfile" << lckFile.data();
-   ::unlink(tmpFile);
-   return KLockFile::LockFail;
+    // Failed to delete stale lock file
+    kWarning() << "Problem deleting stale lockfile" << lckFile.data();
+    ::unlink(tmpFile);
+    return KLockFile::LockFail;
 }
 
 
 KLockFile::LockResult KLockFile::lock(LockFlags options)
 {
-  if (d->isLocked)
-     return KLockFile::LockOK;
+    if (d->isLocked) {
+        return KLockFile::LockOK;
+    }
 
-  KLockFile::LockResult result;
-  int hardErrors = 5;
-  int n = 5;
-  while(true)
-  {
+    KLockFile::LockResult result;
+    int hardErrors = 5;
+    int n = 5;
+    while(true) {
         KDE_struct_stat st_buf;
         // Try to create the lock file
         result = d->lockFile(st_buf);
 
-     if (result == KLockFile::LockOK)
-     {
-        d->staleTimer = QTime();
-        break;
-     }
-     else if (result == KLockFile::LockError)
-     {
-        d->staleTimer = QTime();
-        if (--hardErrors == 0)
-        {
-           break;
+        if (result == KLockFile::LockOK) {
+            d->staleTimer = QTime();
+            break;
+        } else if (result == KLockFile::LockError) {
+            d->staleTimer = QTime();
+            if (--hardErrors == 0) {
+                break;
+            }
+        } else {
+            // KLockFile::Fail -- there is already such a file present (e.g. left by a crashed app)
+            if (!d->staleTimer.isNull() && d->statBuf != st_buf) {
+                d->staleTimer = QTime();
+            }
+
+            if (d->staleTimer.isNull()) {
+                memcpy(&(d->statBuf), &st_buf, sizeof(KDE_struct_stat));
+                d->staleTimer.start();
+
+                d->readLockFile();
+            }
+
+            bool isStale = false;
+            if ((d->m_pid > 0) && !d->m_hostname.isEmpty()) {
+                // Check if hostname is us
+                if (d->m_hostname == QHostInfo::localHostName()) {
+                    // Check if pid still exists
+                    int res = ::kill(d->m_pid, 0);
+                    if ((res == -1) && (errno == ESRCH)) {
+                        isStale = true; // pid does not exist
+                    }
+                }
+            }
+            if (d->staleTimer.elapsed() > (d->staleTime*1000)) {
+                isStale = true;
+            }
+
+            if (isStale) {
+                if ((options & ForceFlag) == 0) {
+                    return KLockFile::LockStale;
+                }
+
+                result = d->deleteStaleLock();
+
+                if (result == KLockFile::LockOK) {
+                    // Lock deletion successful
+                    d->staleTimer = QTime();
+                    continue; // Now try to get the new lock
+                } else if (result != KLockFile::LockFail) {
+                    return result;
+                }
+            }
         }
-     }
-     else // KLockFile::Fail -- there is already such a file present (e.g. left by a crashed app)
-     {
-        if (!d->staleTimer.isNull() && d->statBuf != st_buf)
-           d->staleTimer = QTime();
 
-        if (d->staleTimer.isNull())
-        {
-           memcpy(&(d->statBuf), &st_buf, sizeof(KDE_struct_stat));
-           d->staleTimer.start();
-
-           d->readLockFile();
+        if (options & NoBlockFlag) {
+            break;
         }
 
-        bool isStale = false;
-        if ((d->m_pid > 0) && !d->m_hostname.isEmpty())
-        {
-           // Check if hostname is us
-           if (d->m_hostname == QHostInfo::localHostName())
-           {
-              // Check if pid still exists
-              int res = ::kill(d->m_pid, 0);
-              if ((res == -1) && (errno == ESRCH))
-                  isStale = true; // pid does not exist
-           }
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = n*((KRandom::random() % 200)+100);
+        if (n < 2000) {
+            n = n * 2;
         }
-        if (d->staleTimer.elapsed() > (d->staleTime*1000))
-           isStale = true;
 
-        if (isStale)
-        {
-           if ((options & ForceFlag) == 0)
-              return KLockFile::LockStale;
-
-           result = d->deleteStaleLock();
-
-           if (result == KLockFile::LockOK)
-           {
-              // Lock deletion successful
-              d->staleTimer = QTime();
-              continue; // Now try to get the new lock
-           }
-           else if (result != KLockFile::LockFail)
-           {
-              return result;
-           }
-        }
-     }
-
-     if (options & NoBlockFlag)
-        break;
-
-     struct timeval tv;
-     tv.tv_sec = 0;
-     tv.tv_usec = n*((KRandom::random() % 200)+100);
-     if (n < 2000)
-        n = n * 2;
-
-     select(0, 0, 0, 0, &tv);
-  }
-  if (result == LockOK)
-     d->isLocked = true;
-  return result;
+        select(0, 0, 0, 0, &tv);
+    }
+    if (result == LockOK) {
+        d->isLocked = true;
+    }
+    return result;
 }
 
 bool KLockFile::isLocked() const
 {
-  return d->isLocked;
+    return d->isLocked;
 }
 
 void KLockFile::unlock()
 {
-  if (d->isLocked)
-  {
-     ::unlink(QFile::encodeName(d->m_fileName));
-      if (d->mustCloseFd) {
-         close(d->m_file.handle());
-         d->mustCloseFd = false;
-     }
-     d->m_file.close();
-     d->m_pid = -1;
-     d->isLocked = false;
-  }
+    if (d->isLocked) {
+        ::unlink(QFile::encodeName(d->m_fileName));
+        if (d->mustCloseFd) {
+            close(d->m_file.handle());
+            d->mustCloseFd = false;
+        }
+        d->m_file.close();
+        d->m_pid = -1;
+        d->isLocked = false;
+    }
 }
 
 bool KLockFile::getLockInfo(int &pid, QString &hostname, QString &appname)
 {
-  if (d->m_pid == -1)
-     return false;
-  pid = d->m_pid;
-  hostname = d->m_hostname;
-  appname = d->m_componentName;
-  return true;
+    if (d->m_pid == -1) {
+        return false;
+    }
+    pid = d->m_pid;
+    hostname = d->m_hostname;
+    appname = d->m_componentName;
+    return true;
 }
