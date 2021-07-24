353e68f855cc32b75fdd6cd99400bc7fc401a669
Ivailo Monev <xakepa10@gmail.com>
2021-07-24 17:54:16 +0300

    kio: set maximum remote file size for previews to 1MB by default
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kio/kio/previewjob.cpp
+++ kio/kio/previewjob.cpp
@@ -51,6 +51,12 @@
 namespace KIO { struct PreviewItem; }
 using namespace KIO;
 
+// NOTE: keep in sync with kde-baseapps/dolphin/src/settings/general/previewssettingspage.cpp
+enum MaxPreviewSizes {
+    MaxLocalSize = 5, // 5 MB
+    MaxRemoteSize = 1 // 1 MB
+};
+
 struct KIO::PreviewItem
 {
     KFileItem item;
@@ -315,8 +321,8 @@ void PreviewJobPrivate::startPreview()
     }
 
     KConfigGroup cg( KGlobal::config(), "PreviewSettings" );
-    maximumLocalSize = cg.readEntry( "MaximumSize", 5*1024*1024LL /* 5MB */ );
-    maximumRemoteSize = cg.readEntry( "MaximumRemoteSize", 0 );
+    maximumLocalSize = cg.readEntry( "MaximumSize", MaxPreviewSizes::MaxLocalSize *1024 * 1024LL);
+    maximumRemoteSize = cg.readEntry( "MaximumRemoteSize", MaxPreviewSizes::MaxRemoteSize *1024 * 1024LL );
 
     if (bNeedCache)
     {
