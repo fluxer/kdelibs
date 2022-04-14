65fd9ee0375531ffa671f537225860ea9e623db6
Ivailo Monev <xakepa10@gmail.com>
2022-04-14 09:56:49 +0300

    cmake: try pkg-config lookup first from FFmpeg module
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- cmake/modules/FindFFmpeg.cmake
+++ cmake/modules/FindFFmpeg.cmake
@@ -9,42 +9,62 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-find_path(LIBAVCODEC_INCLUDES
-    NAMES libavcodec/codec_desc.h
-    HINTS $ENV{FFMPEGDIR}/include
-)
+if(NOT WIN32)
+    include(FindPkgConfig)
+    pkg_check_modules(PC_LIBAVCODEC QUIET libavcodec)
+    pkg_check_modules(PC_LIBAVFORMAT QUIET libavformat)
+    pkg_check_modules(PC_LIBAVUTIL QUIET libavutil)
 
-find_library(LIBAVCODEC_LIBRARY
-    NAMES avcodec
-    HINTS $ENV{FFMPEGDIR}/lib
-)
+    set(LIBAVCODEC_INCLUDES ${PC_LIBAVCODEC_INCLUDE_DIRS})
+    set(LIBAVCODEC_LIBRARIES ${PC_LIBAVCODEC_LIBRARIES})
+    set(LIBAVFORMAT_INCLUDES ${PC_LIBAVFORMAT_INCLUDE_DIRS})
+    set(LIBAVFORMAT_LIBRARIES ${PC_LIBAVFORMAT_LIBRARIES})
+    set(LIBAVUTIL_INCLUDES ${PC_LIBAVUTIL_INCLUDE_DIRS})
+    set(LIBAVUTIL_LIBRARIES ${PC_LIBAVUTIL_LIBRARIES})
+endif()
 
-find_path(LIBAVFORMAT_INCLUDES
-    NAMES libavformat/avformat.h
-    HINTS $ENV{FFMPEGDIR}/include
-)
+if(NOT LIBAVCODEC_INCLUDES OR NOT LIBAVCODEC_LIBRARIES)
+    find_path(LIBAVCODEC_INCLUDES
+        NAMES libavcodec/codec_desc.h
+        HINTS $ENV{FFMPEGDIR}/include
+    )
 
-find_library(LIBAVFORMAT_LIBRARY
-    NAMES avformat
-    HINTS $ENV{FFMPEGDIR}/lib
-)
+    find_library(LIBAVCODEC_LIBRARIES
+        NAMES avcodec
+        HINTS $ENV{FFMPEGDIR}/lib
+    )
+endif()
 
-find_path(LIBAVUTIL_INCLUDES
-    NAMES libavutil/dict.h
-    HINTS $ENV{FFMPEGDIR}/include
-)
+if(NOT LIBAVFORMAT_INCLUDES OR NOT LIBAVFORMAT_LIBRARIES)
+    find_path(LIBAVFORMAT_INCLUDES
+        NAMES libavformat/avformat.h
+        HINTS $ENV{FFMPEGDIR}/include
+    )
 
-find_library(LIBAVUTIL_LIBRARY
-    NAMES avformat
-    HINTS $ENV{FFMPEGDIR}/lib
-)
+    find_library(LIBAVFORMAT_LIBRARIES
+        NAMES avformat
+        HINTS $ENV{FFMPEGDIR}/lib
+    )
+endif()
+
+if(NOT LIBAVUTIL_INCLUDES OR NOT LIBAVUTIL_LIBRARIES)
+    find_path(LIBAVUTIL_INCLUDES
+        NAMES libavutil/dict.h
+        HINTS $ENV{FFMPEGDIR}/include
+    )
+
+    find_library(LIBAVUTIL_LIBRARIES
+        NAMES avformat
+        HINTS $ENV{FFMPEGDIR}/lib
+    )
+endif()
 
 if (LIBAVCODEC_INCLUDES AND LIBAVFORMAT_INCLUDES AND LIBAVUTIL_INCLUDES)
     set(FFMPEG_INCLUDES ${LIBAVCODEC_INCLUDES} ${LIBAVFORMAT_INCLUDES} ${LIBAVUTIL_INCLUDES})
 endif()
 
-if (LIBAVCODEC_LIBRARY AND LIBAVFORMAT_LIBRARY AND LIBAVUTIL_LIBRARY)
-    set(FFMPEG_LIBRARIES ${LIBAVCODEC_LIBRARY} ${LIBAVFORMAT_LIBRARY} ${LIBAVUTIL_LIBRARY})
+if (LIBAVCODEC_LIBRARIES AND LIBAVFORMAT_LIBRARIES AND LIBAVUTIL_LIBRARIES)
+    set(FFMPEG_LIBRARIES ${LIBAVCODEC_LIBRARIES} ${LIBAVFORMAT_LIBRARIES} ${LIBAVUTIL_LIBRARIES})
 endif()
 
 include(FindPackageHandleStandardArgs)
