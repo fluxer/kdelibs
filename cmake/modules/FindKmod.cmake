b089f32459b6bbb43aa07da25233c4f16df34dfe
Ivailo Monev <xakepa10@gmail.com>
2022-05-01 15:16:15 +0300

    cmake: new Kmod module
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ cmake/modules/FindKmod.cmake
@@ -0,0 +1,40 @@
+# Try to find kmod library, once done this will define:
+#
+#  KMOD_FOUND - system has kmod
+#  KMOD_INCLUDE_DIR - the kmod include directory
+#  KMOD_LIBRARIES - the libraries needed to use kmod
+#
+# Copyright (c) 2022 Ivailo Monev <xakepa10@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+if(NOT WIN32)
+    include(FindPkgConfig)
+    pkg_check_modules(PC_KMOD QUIET libkmod)
+
+    set(KMOD_INCLUDE_DIR ${PC_KMOD_INCLUDE_DIRS})
+    set(KMOD_LIBRARIES ${PC_KMOD_LIBRARIES})
+endif()
+
+set(KMOD_VERSION ${PC_KMOD_VERSION})
+
+if(NOT KMOD_INCLUDE_DIR OR NOT KMOD_LIBRARIES)
+    find_path(KMOD_INCLUDE_DIR
+        NAMES libkmod.h
+        HINTS $ENV{KMODDIR}/include
+    )
+
+    find_library(KMOD_LIBRARIES
+        NAMES kmod
+        HINTS $ENV{KMODDIR}/lib
+    )
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Kmod
+    VERSION_VAR KMOD_VERSION
+    REQUIRED_VARS KMOD_LIBRARIES KMOD_INCLUDE_DIR
+)
+
+mark_as_advanced(KMOD_INCLUDE_DIR KMOD_LIBRARIES)
