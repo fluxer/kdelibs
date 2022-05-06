2d373d45efe5db395287a61909865ff1dadeaa9b
Ivailo Monev <xakepa10@gmail.com>
2022-05-06 15:26:19 +0300

    kutils: document KDNSSDService and KDNSSD, mark them as stable
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kutils/kdnssd/kdnssd.h
+++ kutils/kdnssd/kdnssd.h
@@ -24,6 +24,24 @@
 #include <QObject>
 #include <QList>
 
+/*!
+    Structure that holds information about discovarable service.
+
+    @note The @p type member is in the form _\<service\>._\<sockettype\> where service is one as
+    described at @link http://www.dns-sd.org/ServiceTypes.html and sockettype is usually either
+    "tcp" or "udp".
+
+    @note The @p url member is synthetized URL in the form of \<service\>://\<hostname\>:\<port\>
+    and it may not be accessible via KIO. For example there is no KIO slave for "sftp-ssh" service
+    however there is KIO slave that can handle SFTP (SSH) protocol and @p url may have to adjusted
+    before attempting to open it with external program. In addition to that,
+    @p KProtocolInfo::isKnownProtocol() may be used to check if there is KIO slave that can handle
+    @p url since some KIO slaves are optional (such as the SFTP (SSH) slave).
+
+    @since 4.21
+    @see KDNSSD
+    @link http://www.dns-sd.org/ServiceTypes.html
+*/
 struct KDNSSDService
 {
     QString name;
@@ -37,10 +55,11 @@ struct KDNSSDService
 class KDNSSDPrivate;
 
 /*!
-    Class to register and query for discovarable services.
+    Class to publish and query for discovarable services.
 
     @since 4.21
-    @warning the API is subject to change
+    @see KDNSSDService
+    @link http://www.dns-sd.org/ServiceTypes.html
 */
 class KDNSSD_EXPORT KDNSSD : public QObject
 {
@@ -52,12 +71,25 @@ public:
     KDNSSD(QObject *parent = nullptr);
     ~KDNSSD();
 
+    /*!
+        @brief Publishes service of type @p servicetype on port @p serviceport with name
+        @p servicename on the default domain.
+        @note Only one service may be published per KDNSSD object. To publish multiple services
+        a new KDNSSD object must be constructed.
+    */
     bool publishService(const QByteArray &servicetype, const uint serviceport, const QString &servicename);
+    /*!
+        @brief Unpublishes service previously published via @p publishService().
+        @note Service published via @p publishService() is automatically unpublished once the
+        KDNSSD object is destroyed.
+    */
     bool unpublishService();
 
     /*!
         @brief Starts browse request for @p servicetype, if @p servicetype is empty the result will
-        include services for all service types. This will block until all services are resolved
+        include services for all service types. This will block until all services are resolved.
+        When the query is done @p finished() signal is emited and the result can be retrieved via
+        the @p services() method.
     */
     void startBrowse(const QByteArray &servicetype = QByteArray());
     QList<KDNSSDService> services() const;
