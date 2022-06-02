3c078e35790af6bc2eecb24550e8f31a2e592464
Ivailo Monev <xakepa10@gmail.com>
2022-06-03 01:37:20 +0300

    kio: remove unused slave definitions
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kio/kio/ioslave_defaults.h
+++ kio/kio/ioslave_defaults.h
@@ -29,23 +29,11 @@
 // MINMUM SIZE FOR ABORTED DOWNLOAD TO BE KEPT
 #define DEFAULT_MINIMUM_KEEP_SIZE         5120  //  5 Kbs
 
-// NORMAL PORT DEFAULTS
+// PORT DEFAULTS
 #define DEFAULT_FTP_PORT                    21
 #define DEFAULT_SFTP_PORT                   22
-#define DEFAULT_SMTP_PORT                   25
 #define DEFAULT_HTTP_PORT                   80
-#define DEFAULT_POP3_PORT                  110
-#define DEFAULT_NNTP_PORT                  119
-#define DEFAULT_IMAP_PORT                  143
-#define DEFAULT_IMAP3_PORT                 220
-#define DEFAULT_LDAP_PORT                  389
-
-// SECURE PORT DEFAULTS
 #define DEFAULT_HTTPS_PORT                 443
-#define DEFAULT_NNTPS_PORT                 563
-#define DEFAULT_LDAPS_PORT                 389
-#define DEFAULT_IMAPS_PORT                 993
-#define DEFAULT_POP3S_PORT                 995
 
 // OTHER GENERIC PORT DEFAULTS
 #define DEFAULT_PROXY_PORT                8080
