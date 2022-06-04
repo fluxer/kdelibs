adda86c541c216810323b30873c5fb9e49db38c0
Ivailo Monev <xakepa10@gmail.com>
2022-06-04 22:28:09 +0300

    kio: remove unused KIO::UDSEntry object in PreviewJobPrivate::statResultThumbnail()
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kio/kio/previewjob.cpp
+++ kio/kio/previewjob.cpp
@@ -501,7 +501,6 @@ bool PreviewJobPrivate::statResultThumbnail()
 
     // NOTE: make sure the algorithm and name match those used in kde-workspace/kioslave/thumbnail/thumbnail.cpp
     const QByteArray hash = QFile::encodeName( origName ).toHex();
-    KIO::UDSEntry thumbEntry;
     const QString modTime = QString::number(QFileInfo(url.prettyUrl()).lastModified().toTime_t());
     thumbName = hash + modTime + thumbExt;
 
