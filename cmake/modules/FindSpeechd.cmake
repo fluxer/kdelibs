4ba4c820b50dda344f1e986de817ce4fa1946936
Ivailo Monev <xakepa10@gmail.com>
2022-09-26 09:49:55 +0300

    cmake: remove commas in copyright note in Speechd module
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- cmake/modules/FindSpeechd.cmake
+++ cmake/modules/FindSpeechd.cmake
@@ -4,7 +4,7 @@
 #  SPEECHD_INCLUDE_DIR - the speech-dispatcher include directory
 #  SPEECHD_LIBRARIES - the libraries needed to use speech-dispatcher
 #
-# Copyright (c) 2020, Ivailo Monev, <xakepa10@gmail.com>
+# Copyright (c) 2020 Ivailo Monev <xakepa10@gmail.com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
