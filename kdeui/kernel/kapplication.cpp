5336ff3054a0c11c7cc3a1a6fc28b0a60af959cc
Ivailo Monev <xakepa10@gmail.com>
2022-10-16 23:58:09 +0300

    kdeui: stdlib is not included for rand() and srand() in kapplication source file
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdeui/kernel/kapplication.cpp
+++ kdeui/kernel/kapplication.cpp
@@ -62,7 +62,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include <stdlib.h> // srand(), rand()
+#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/stat.h>
