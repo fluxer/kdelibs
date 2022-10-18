17cef65ed7fa1e2d45591ac838d40294d5036120
Ivailo Monev <xakepa10@gmail.com>
2022-10-19 02:45:30 +0300

    kdeui: somewhat format and indent kxerrorhandlertest source file
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdeui/tests/kxerrorhandlertest.cpp
+++ kdeui/tests/kxerrorhandlertest.cpp
@@ -29,19 +29,19 @@ using namespace std;
 #include <kxerrorhandler.h>
 
 int handler1( Display*, XErrorEvent* e )
-    {
+{
     cout << "ERR1:" << e->resourceid << ":" << (int)e->error_code << ":" << (int)e->request_code << ":" << e->serial << endl;
     return 1;
-    }
+}
 
 bool handler3( int request, int error_code, unsigned long resourceid )
-    {
+{
     cout << "ERR3:" << resourceid << ":" << error_code << ":" << request << endl;
     return true;
-    }
+}
     
 int main()
-    {
+{
     Display* dpy = XOpenDisplay( NULL );
     XSetWindowAttributes attrs;
     Window w = XCreateWindow( dpy, DefaultRootWindow( dpy ), 0, 0, 100, 100, 0, CopyFromParent, CopyFromParent,
@@ -50,23 +50,22 @@ int main()
     XMapWindow( dpy, w );
     ++w;
 //    XSetInputFocus( dpy, w, RevertToParent, CurrentTime );
-        {
+    {
         KXErrorHandler handle1( handler1, dpy );
         cout << w << ":" << XNextRequest( dpy ) << endl;
         XMapWindow( dpy, w );
         XWindowAttributes attr;
-            {
+        {
             KXErrorHandler handle2( dpy );
             XGetWindowAttributes(dpy, w, &attr);
             cout << "WAS2:" << handle2.error( false ) << endl;
-            }
+        }
 //        XSync( dpy, False );
         cout << "WAS1:" << handle1.error( false ) << endl;
-        }
-    for(;;)
-        {
+    }
+    for(;;) {
         XEvent ev;
         XNextEvent( dpy, &ev );
-        }
-    XCloseDisplay( dpy );
     }
+    XCloseDisplay( dpy );
+}
