272adc7db110cc347de3b71c7083bdd193f1ecc0
Ivailo Monev <xakepa10@gmail.com>
2022-10-19 01:44:56 +0300

    kdeui: remove note about KPixmapRegionSelectorWidget that no longer applies
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdeui/widgets/kpixmapregionselectorwidget.h
+++ kdeui/widgets/kpixmapregionselectorwidget.h
@@ -32,11 +32,6 @@ class KMenu;
  * KPixmapRegionSelectorWidget is a widget that shows a picture and provides the
  * user with a friendly way to select a rectangular subregion of the pixmap.
  *
- * NOTE: There are two copies of this .h and the .cpp file, with subtle differences.
- * One copy is in kdelibs/kdeui, and the other copy is in kdepim/libkdepim
- * This is because kdepim has to remain backwards compatible.  Any changes
- * to either file should be made to the other.
- *
  * \image html kpixmapregionselectorwidget.png "KDE Pixmap Region Selector"
  *
  * @author Antonio Larrosa <larrosa@kde.org>
