46f8fe92c4de0d0be76b3c9e82b949e2fc8fc49b
Ivailo Monev <xakepa10@gmail.com>
2022-10-20 05:24:44 +0300

    cmake: libdeflate now provides pkg-config file
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- cmake/modules/FindLibDeflate.cmake
+++ cmake/modules/FindLibDeflate.cmake
@@ -9,22 +9,28 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-# libdeflate does not provide pkg-config files
-
 find_package(PkgConfig REQUIRED)
+pkg_check_modules(PC_LIBDEFLATE QUIET libdeflate)
+
+set(LIBDEFLATE_INCLUDES ${PC_LIBDEFLATE_INCLUDE_DIRS})
+set(LIBDEFLATE_LIBRARIES ${PC_LIBDEFLATE_LIBRARIES})
+set(LIBDEFLATE_VERSION ${PC_LIBDEFLATE_VERSION})
+
+if(NOT LIBDEFLATE_INCLUDES OR NOT LIBDEFLATE_LIBRARIES)
+    find_path(LIBDEFLATE_INCLUDES
+        NAMES libdeflate.h
+        HINTS $ENV{LIBDEFLATEDIR}/include
+    )
+
+    find_library(LIBDEFLATE_LIBRARIES
+        NAMES deflate
+        HINTS $ENV{LIBDEFLATEDIR}/lib
+    )
+endif()
+
 include(FindPackageHandleStandardArgs)
-
-find_path(LIBDEFLATE_INCLUDES
-    NAMES libdeflate.h
-    HINTS $ENV{LIBDEFLATEDIR}/include
-)
-
-find_library(LIBDEFLATE_LIBRARIES
-    NAMES deflate
-    HINTS $ENV{LIBDEFLATEDIR}/lib
-)
-
 find_package_handle_standard_args(LibDeflate
+    VERSION_VAR LIBDEFLATE_VERSION
     REQUIRED_VARS LIBDEFLATE_LIBRARIES LIBDEFLATE_INCLUDES
 )
 
