612b956ada0c67f3f0682ccd3774080bb1e0cfe3
Ivailo Monev <xakepa10@gmail.com>
2022-10-24 04:28:25 +0300

    kdecore: KDebug documentation review
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdecore/io/kdebug.h
+++ kdecore/io/kdebug.h
@@ -23,69 +23,69 @@
 
 #include <QtCore/qdebug.h>
 
-/**
- * \addtogroup kdebug Debug message generators
- *  @{
- * KDE debug message streams let you and the user control just how many debug
- * messages you see. Debug message can be controled by editing kdebugrc and
- * by setting or unsetting environment variables:
- *  KDE_DEBUG_TIMESTAMP - adds timestamp to the message
- *  KDE_DEBUG_METHODNAME - adds the method to the message
- *  KDE_DEBUG_COLOR - colorizes the message, valid only for shell type of
- *                    output and when it is TTY
- */
-
-/**
- * \relates KGlobal
- * Returns a backtrace.
- * Note: Hidden symbol visibility may negatively affect the information provided
- * by kBacktrace - you may want to pass -DCXX_VISIBILITY_PRESET=FALSE to CMake
- * to turn hidden symbol visibility off.
- * @param levels the number of levels of the backtrace
- * @return a backtrace
- */
-KDECORE_EXPORT QString kBacktrace(int levels = -1);
-
-/**
- * \relates KGlobal
- * Deletes the kdebugrc cache and therefore forces KDebug to reread the
- * config file
- */
-KDECORE_EXPORT void kClearDebugConfig();
+/*!
+    \addtogroup kdebug Debug message generators
+    @{
+    KDE debug message streams let you and the user control just how many debug
+    messages you see. Debug message can be controled by editing kdebugrc and
+    by setting or unsetting environment variables:
+    KDE_DEBUG_TIMESTAMP - adds timestamp to the message
+    KDE_DEBUG_METHODNAME - adds the method to the message
+    KDE_DEBUG_COLOR - colorizes the message, valid only for shell type of
+                      output and when it is TTY
+*/
 
 #ifndef KDE_DEFAULT_DEBUG_AREA
 # define KDE_DEFAULT_DEBUG_AREA 0
 #endif
 
 /*!
-  \macro KDE_DEFAULT_DEBUG_AREA
-  \relates KGlobal
+    \macro KDE_DEFAULT_DEBUG_AREA
 
-  Denotes the debug area to use in kDebug/kWarning etc when not
-  explicitly specified. The default is 0 (zero).
+    @brief Denotes the debug area to use in kDebug/kWarning etc when not
+    explicitly specified. The default is 0 (zero).
 
-  Define this macro to the debug area of your application/component
-  before including any KDE headers. Usually, you want to add code like
-  this to your \c CMakeLists.txt:
+    @note Define this macro to the debug area of your application/component
+    before including any KDE headers. Usually, you want to add code like this
+    to your \c CMakeLists.txt:
 
-  \code
-  ...
-  add_definitions( -DKDE_DEFAULT_DEBUG_AREA=1234 )
-  ...
-  \endcode
+    \code
+    ...
+    add_definitions(-DKDE_DEFAULT_DEBUG_AREA=1234)
+    ...
+    \endcode
 
-  This way, you save repeating the debug area all over your source
-  code, in each debug/warning statement.
+    This way, you save repeating the debug area all over your source code in
+    each debug/warning statement.
 */
 
-/**
- * \relates KGlobal
- * Returns a debug stream. You can use it to print debug
- * information.
- * @param type type of message
- * @param funcinfo caller of KDebug
- * @param area an id to identify the output
- */
+/*!
+    @brief Returns a backtrace.
+
+    @note Hidden symbol visibility may negatively affect the information
+    provided by kBacktrace - you may want to pass -DCXX_VISIBILITY_PRESET=FALSE
+    to CMake to turn hidden symbol visibility off.
+
+    @param levels the number of levels of the backtrace
+    @return a backtrace
+*/
+KDECORE_EXPORT QString kBacktrace(int levels = -1);
+
+/*!
+    @brief Clears the KDebug cache and therefore forces it to reread the config
+    file and environment variables.
+*/
+KDECORE_EXPORT void kClearDebugConfig();
+
+/*!
+    @brief Returns a debug stream. In most cases you do not want to use this
+    function, Use the convenience kDebug(), kWarning(), kError() and kFatal()
+    macros instead.
+
+    @param type type of message
+    @param funcinfo caller of KDebug
+    @param area an id to identify the output
+*/
 KDECORE_EXPORT QDebug KDebug(const QtMsgType type, const char* const funcinfo, const int area = KDE_DEFAULT_DEBUG_AREA);
 
 // operators for KDE types
@@ -99,16 +99,20 @@ KDECORE_EXPORT QDebug operator<<(QDebug s, const KDateTime &time);
 #define kError(...)     KDebug(QtCriticalMsg, Q_FUNC_INFO, ##__VA_ARGS__)
 #define kFatal(...)     KDebug(QtFatalMsg, Q_FUNC_INFO, ##__VA_ARGS__)
 
-/**
- * Convenience macro, use this to remind yourself to finish the implementation of a function
- * @since 4.6
- */
+/*!
+    @brief Convenience macro, use this to remind yourself to finish the
+    implementation of a function.
+
+    @since 4.6
+*/
 #define KWARNING_NOTIMPLEMENTED kWarning() << "NOT-IMPLEMENTED" << Q_FUNC_INFO;
 
-/**
- * Convenience macro, use this to alert other developers to stop using a function
- * @since 4.6
- */
+/*!
+    @brief Convenience macro, use this to alert other developers to stop using
+    a function.
+
+    @since 4.6
+*/
 #define KWARNING_DEPRECATED kWarning() << "DEPRECATED" << Q_FUNC_INFO;
 
 /** @} */
