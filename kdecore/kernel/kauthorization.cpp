b0b0f528daad4f1e9d1fccc849d4584ab794e8bd
Ivailo Monev <xakepa10@gmail.com>
2022-12-08 01:29:04 +0200

    kdecore: parent the KAuthorization object to the application instance if parent is null
    that way it will be deleted before the application instance is plugging
    memory leaks
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdecore/kernel/kauthorization.cpp
+++ kdecore/kernel/kauthorization.cpp
@@ -127,7 +127,7 @@ void KAuthorizationAdaptor::stop()
 
 
 KAuthorization::KAuthorization(QObject *parent)
-    : QObject(parent)
+    : QObject(parent ? parent : qApp)
 {
     new KAuthorizationAdaptor(this);
 }
