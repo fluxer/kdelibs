890dfa808decc3134be0c8c8da0a1a5f7c03c1b8
Ivailo Monev <xakepa10@gmail.com>
2022-12-22 01:04:49 +0200

    kded: prefix the applications menu with kde-
    according to the spec it may be prefixed, if it is XDG_MENU_PREFIX must
    be exported (which matters not to Katana since the main application menu
    file is hardcoded anyway). for reference:
    https://specifications.freedesktop.org/menu-spec/menu-spec-1.0.html
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kded/kbuildsycoca.cpp
+++ kded/kbuildsycoca.cpp
@@ -307,7 +307,7 @@ bool KBuildSycoca::build()
      if (!m_trackId.isEmpty())
         g_vfolder->setTrackId(m_trackId);
 
-     VFolderMenu::SubMenu *kdeMenu = g_vfolder->parseMenu("applications.menu");
+     VFolderMenu::SubMenu *kdeMenu = g_vfolder->parseMenu("kde-applications.menu");
 
      KServiceGroup::Ptr entry = g_buildServiceGroupFactory->addNew("/", kdeMenu->directoryFile, KServiceGroup::Ptr(), false);
      entry->setLayoutInfo(kdeMenu->layoutList);
