39ea2ed450f37a52daec5dbdfdca9625eb6aea27
Ivailo Monev <xakepa10@gmail.com>
2023-05-22 03:23:35 +0300

    plasma: remove unused headers inclusions
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- plasma/private/windowpreview.cpp
+++ plasma/private/windowpreview.cpp
@@ -30,13 +30,6 @@
 #include <plasma/framesvg.h>
 #include <plasma/windoweffects.h>
 
-#ifdef Q_WS_X11
-#include <QtGui/qx11info_x11.h>
-
-#include <X11/Xlib.h>
-#include <fixx11h.h>
-#endif
-
 namespace Plasma {
 
 WindowPreview::WindowPreview(QWidget *parent)
--- plasma/tooltipmanager.cpp
+++ plasma/tooltipmanager.cpp
@@ -32,13 +32,6 @@
 //KDE
 #include <kwindowsystem.h>
 
-//X11
-#ifdef Q_WS_X11
-#include <QtGui/qx11info_x11.h>
-#include <X11/Xlib.h>
-#include <fixx11h.h>
-#endif
-
 //Plasma
 #include "plasma/applet.h"
 #include "plasma/containment.h"
