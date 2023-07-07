8bf6e8bd73f6fa2bf595036a659fafdcfbb765db
Ivailo Monev <xakepa10@gmail.com>
2023-07-07 07:51:47 +0300

    kdeui: do not specify margin for the label of KMessageWidget
    there is no inner frame now
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdeui/widgets/kmessagewidget.cpp
+++ kdeui/widgets/kmessagewidget.cpp
@@ -227,7 +227,6 @@ void KMessageWidget::setMessageType(KMessageWidget::MessageType type)
             "    stop: 1.0 %3);"
             "border-radius: 5px;"
             "border: 1px solid %4;"
-            "margin: %5px;"
             "color: %6;"
             "}"
             )
@@ -235,8 +234,6 @@ void KMessageWidget::setMessageType(KMessageWidget::MessageType type)
         .arg(bg1.name())
         .arg(bg2.name())
         .arg(border.name())
-        // DefaultFrameWidth returns the size of the external margin + border width. We know our border is 1px, so we subtract this from the frame normal QStyle FrameWidth to get our margin
-        .arg(style()->pixelMetric(QStyle::PM_DefaultFrameWidth, 0, this) -1)
         .arg(fg.name())
     );
 }
