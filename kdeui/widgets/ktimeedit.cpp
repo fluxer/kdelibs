9642bc3d819f9cb5c115891b555eb968d631c6e6
Ivailo Monev <xakepa10@gmail.com>
2023-08-03 01:33:35 +0300

    kdeui: handle zero time as special case in KTimeEdit and better min/max
    the plasma frame applet for example uses zero time (QTime(0, 0, 0), not
    invalid time) as a special for case for "never" (i.e. don't update) so
    having this feature will make KTimeEdit even more suitable for timers (that
    ofcourse has to be handled by the application or library, zero time timers
    are a thing)
    
    also min/max now allows to set lower seconds (for example) when the minute
    is not zero instead of just restricting the seconds value to a hard-minumum
    altough that makes me think a step feature may be needed (e.g. to now allow
    intervals less than two seconds with minimum of one second)
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kdeui/widgets/ktimeedit.cpp
+++ kdeui/widgets/ktimeedit.cpp
@@ -23,38 +23,105 @@
 #include <QSpinBox>
 #include <QHBoxLayout>
 
+class KTimeEditPrivate;
+
+class KTimeBox : public QSpinBox
+{
+    Q_OBJECT
+public:
+    KTimeBox(KTimeEditPrivate *privateparent, QWidget *parent);
+
+protected:
+    QValidator::State validate(QString &input, int &pos) const final;
+
+private:
+    KTimeEditPrivate* ktimeeditprivate;
+};
+
+KTimeBox::KTimeBox(KTimeEditPrivate *privateparent, QWidget *parent)
+    : QSpinBox(parent),
+    ktimeeditprivate(privateparent)
+{
+}
+
+
 class KTimeEditPrivate
 {
 public:
     KTimeEditPrivate(KTimeEdit *parent);
 
-    void updateSuffixes();
+    void updateWidgets();
     void slotValueChanged(const int value);
 
     KTimeEdit* ktimeedit;
-    QSpinBox* hourbox;
-    QSpinBox* minutebox;
-    QSpinBox* secondbox;
+    KTimeBox* hourbox;
+    KTimeBox* minutebox;
+    KTimeBox* secondbox;
+    QTime mintime;
+    QTime maxtime;
 };
+
+
+QValidator::State KTimeBox::validate(QString &input, int &pos) const
+{
+    const int hour = ktimeeditprivate->hourbox->value();
+    const int minute = ktimeeditprivate->minutebox->value();
+    const int second = ktimeeditprivate->secondbox->value();
+    const QTime currenttime = QTime(hour, minute, second);
+    if (!currenttime.isValid()) {
+        return QValidator::Invalid;
+    }
+    if (currenttime < ktimeeditprivate->mintime || currenttime > ktimeeditprivate->maxtime) {
+        return QValidator::Invalid;
+    }
+    return QValidator::Acceptable;
+}
+
 
 KTimeEditPrivate::KTimeEditPrivate(KTimeEdit *parent)
     : ktimeedit(parent),
     hourbox(nullptr),
     minutebox(nullptr),
-    secondbox(nullptr)
+    secondbox(nullptr),
+    mintime(0, 0, 0),
+    maxtime(23, 59, 59)
 {
 }
 
-void KTimeEditPrivate::updateSuffixes()
+void KTimeEditPrivate::updateWidgets()
 {
-    hourbox->setSuffix(i18np(" hour", " hours", hourbox->value()));
-    minutebox->setSuffix(i18np(" minute", " minutes", minutebox->value()));
-    secondbox->setSuffix(i18np(" second", " seconds", secondbox->value()));
+    const int hour = hourbox->value();
+    const int minute = minutebox->value();
+    const int second = secondbox->value();
+    hourbox->setSuffix(i18np(" hour", " hours", hour));
+    minutebox->setSuffix(i18np(" minute", " minutes", minute));
+    secondbox->setSuffix(i18np(" second", " seconds", second));
+    if (hour == 0 && minute == 0 && second == 0) {
+        hourbox->setSpecialValueText(i18n("never"));
+        minutebox->setSpecialValueText(i18n("never"));
+        secondbox->setSpecialValueText(i18n("never"));
+    } else {
+        hourbox->setSpecialValueText(QString());
+        minutebox->setSpecialValueText(QString());
+        secondbox->setSpecialValueText(QString());
+    }
+    hourbox->setMinimum(mintime.hour());
+    hourbox->setMaximum(maxtime.hour());
+    if (hour > 0) {
+        minutebox->setMinimum(0);
+    } else {
+        minutebox->setMinimum(mintime.minute());
+    }
+    if (hour > 0 || minute > 0) {
+        secondbox->setMinimum(0);
+    } else {
+        secondbox->setMinimum(mintime.second());
+    }
 }
 
 void KTimeEditPrivate::slotValueChanged(const int value)
 {
-    updateSuffixes();
+    updateWidgets();
     const QTime currenttime = QTime(hourbox->value(), minutebox->value(), secondbox->value());
     emit ktimeedit->timeChanged(currenttime);
 }
@@ -65,22 +132,19 @@ KTimeEdit::KTimeEdit(QWidget *parent)
     d(new KTimeEditPrivate(this))
 {
     QHBoxLayout* timelayout = new QHBoxLayout(this);
-    d->hourbox = new QSpinBox(this);
+    d->hourbox = new KTimeBox(d, this);
     d->hourbox->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-    d->hourbox->setMaximum(23);
     timelayout->addWidget(d->hourbox);
-    d->minutebox = new QSpinBox(this);
+    d->minutebox = new KTimeBox(d, this);
     d->minutebox->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-    d->minutebox->setMaximum(59);
     timelayout->addWidget(d->minutebox);
-    d->secondbox = new QSpinBox(this);
+    d->secondbox = new KTimeBox(d, this);
     d->secondbox->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-    d->secondbox->setMaximum(59);
     timelayout->addWidget(d->secondbox);
     setLayout(timelayout);
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
 
-    d->updateSuffixes();
+    d->updateWidgets();
 
     connect(d->hourbox, SIGNAL(valueChanged(int)), this, SLOT(slotValueChanged(int)));
     connect(d->minutebox, SIGNAL(valueChanged(int)), this, SLOT(slotValueChanged(int)));
@@ -94,26 +158,24 @@ KTimeEdit::~KTimeEdit()
 
 QTime KTimeEdit::minimumTime() const
 {
-    return QTime(d->hourbox->minimum(), d->minutebox->minimum(), d->secondbox->minimum());
+    return d->mintime;
 }
 
 void KTimeEdit::setMinimumTime(const QTime &time)
 {
-    d->hourbox->setMinimum(time.hour());
-    d->minutebox->setMinimum(time.minute());
-    d->secondbox->setMinimum(time.second());
+    d->mintime = time;
+    d->updateWidgets();
 }
 
 QTime KTimeEdit::maximumTime() const
 {
-    return QTime(d->hourbox->maximum(), d->minutebox->maximum(), d->secondbox->maximum());
+    return d->maxtime;
 }
 
 void KTimeEdit::setMaximumTime(const QTime &time)
 {
-    d->hourbox->setMaximum(time.hour());
-    d->minutebox->setMaximum(time.minute());
-    d->secondbox->setMaximum(time.second());
+    d->maxtime = time;
+    d->updateWidgets();
 }
 
 QTime KTimeEdit::time() const
@@ -129,3 +191,4 @@ void KTimeEdit::setTime(const QTime &time)
 }
 
 #include "moc_ktimeedit.cpp"
+#include "ktimeedit.moc"
